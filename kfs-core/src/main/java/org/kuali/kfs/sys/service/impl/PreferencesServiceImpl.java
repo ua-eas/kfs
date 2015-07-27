02408f580ffa8c461d58c252be8e7979f2bb7dca
Bryan Hutchinson <bryan@kuali.co>
2015-07-27 14:58:56 -0400

    KFSTP-2155 - added mongo DAO

--- kfs-core/src/main/java/org/kuali/kfs/sys/service/impl/PreferencesServiceImpl.java
+++ kfs-core/src/main/java/org/kuali/kfs/sys/service/impl/PreferencesServiceImpl.java
@@ -1,6 +1,7 @@
 package org.kuali.kfs.sys.service.impl;
 
 import org.apache.commons.collections.CollectionUtils;
+import org.kuali.kfs.sys.dataaccess.PreferencesDao;
 import org.kuali.kfs.sys.service.PreferencesService;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Query;
@@ -11,25 +12,18 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class PreferencesServiceImpl implements PreferencesService {
 
-    private MongoTemplate mongoTemplate;
+    private PreferencesDao preferencesDao;
 
     @Override
     public Map<String, Object> findInstitutionPreferences() {
-        Map<String, Object> institutionalPreference = new ConcurrentHashMap<>();
-        List<Map> institutionPreferences = mongoTemplate.findAll(Map.class, "InstitutionPreferences");
-
-        if (CollectionUtils.isNotEmpty(institutionPreferences)) {
-            institutionalPreference = institutionPreferences.get(0);
-        }
-        return institutionalPreference;
+        return preferencesDao.findInstitutionPreferences();
     }
 
-    public MongoTemplate getMongoTemplate() {
-        return mongoTemplate;
+    public PreferencesDao getPreferencesDao() {
+        return preferencesDao;
     }
 
-    public void setMongoTemplate(MongoTemplate mongoTemplate) {
-        this.mongoTemplate = mongoTemplate;
+    public void setPreferencesDao(PreferencesDao preferencesDao) {
+        this.preferencesDao = preferencesDao;
     }
-
 }
