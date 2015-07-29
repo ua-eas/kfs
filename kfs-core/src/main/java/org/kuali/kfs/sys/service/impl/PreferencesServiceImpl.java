6e7216c5fd0bb5a49ce01ecc267eca1301c87981
smith750 <jksmith@kuali.co>
2015-07-29 17:49:59 -0400

    KFSTP-2132 actually get transactional documents to build links correctly..i hope...

--- kfs-core/src/main/java/org/kuali/kfs/sys/service/impl/PreferencesServiceImpl.java
+++ kfs-core/src/main/java/org/kuali/kfs/sys/service/impl/PreferencesServiceImpl.java
@@ -1,24 +1,54 @@
 package org.kuali.kfs.sys.service.impl;
 
 import org.apache.commons.collections.CollectionUtils;
+import org.kuali.kfs.sys.KFSConstants;
 import org.kuali.kfs.sys.dataaccess.PreferencesDao;
 import org.kuali.kfs.sys.service.PreferencesService;
+import org.kuali.rice.core.api.config.property.ConfigurationService;
+import org.kuali.rice.krad.bo.ModuleConfiguration;
+import org.kuali.rice.krad.document.Document;
 import org.kuali.rice.krad.service.DocumentDictionaryService;
+import org.kuali.rice.krad.service.KualiModuleService;
+import org.kuali.rice.krad.service.ModuleService;
+import org.kuali.rice.krad.util.ObjectUtils;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Query;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class PreferencesServiceImpl implements PreferencesService {
 
+    private ConfigurationService configurationService;
     private DocumentDictionaryService documentDictionaryService;
+    private KualiModuleService kualiModuleService;
     private PreferencesDao preferencesDao;
+
+    private Map<String, String> namespaceCodeToUrlName;
+
+    public PreferencesServiceImpl() {
+        // initiate defaults for namespaceCodeToUrlName map
+        namespaceCodeToUrlName = new ConcurrentHashMap<>();
+        namespaceCodeToUrlName.put("KFS-AR", "ar");
+        namespaceCodeToUrlName.put("KFS-CAM", "cams");
+        namespaceCodeToUrlName.put("KFS-CAB", "cab");
+        namespaceCodeToUrlName.put("KFS-FP", "financial");
+        namespaceCodeToUrlName.put("KFS-GL", "generalLedger");
+        namespaceCodeToUrlName.put("KFS-LD", "labor");
+        namespaceCodeToUrlName.put("KFS-BC", "budget");
+        namespaceCodeToUrlName.put("KFS-PURAP", "purap");
+        namespaceCodeToUrlName.put("KFS-CG", "cg");
+        namespaceCodeToUrlName.put("KFS-EC", "effort");
+        namespaceCodeToUrlName.put("KFS-TEM", "tem");
+    }
 
     @Override
     public Map<String, Object> findInstitutionPreferences() {
-        final Map<String, Object> institutionPreferences = preferencesDao.findInstitutionPreferences();
+        final Map<String, Object> institutionPreferences = getPreferencesDao().findInstitutionPreferences();
+
+        transformLinks(institutionPreferences);
         
         return institutionPreferences;
     }
@@ -30,7 +60,11 @@ public class PreferencesServiceImpl implements PreferencesService {
     }
 
     protected List<Map<String, Object>> getLinkGroups(Map<String, Object> institutionPreferences) {
-        return (List<Map<String, Object>>)institutionPreferences.get("linkGroups");
+        final List<Map<String, Object>> linkGroups = (List<Map<String, Object>>)institutionPreferences.get("linkGroups");
+        if (!ObjectUtils.isNull(linkGroups)) {
+            return linkGroups;
+        }
+        return new ArrayList<>();
     }
 
     protected void transformLinksInLinkGroup(Map<String, Object> linkGroup) {
@@ -54,11 +88,47 @@ public class PreferencesServiceImpl implements PreferencesService {
 
     protected Map<String, String> determineLinkInfo(String documentTypeName) {
         final String label = getDocumentDictionaryService().getLabel(documentTypeName);
-        // "InternalBillingDocument" -> "http://tst.kfs.kuali.org/kfs-tst/financialInternalBilling.do?methodToCall=docHandler&command=initiate&docTypeName=IB"
+        final Class<? extends Document> documentClass = (Class<? extends Document>)getDocumentDictionaryService().getDocumentClassByName(documentTypeName);
+        final String link = constructTransactionalDocumentLinkFromClass(documentClass, documentTypeName);
 
+        return constructLinkInfo(label, link);
+    }
+
+    protected String constructTransactionalDocumentLinkFromClass(Class<? extends Document> documentClass, String documentTypeName) {
+        final String applicationUrl = getConfigurationService().getPropertyValueAsString(KFSConstants.APPLICATION_URL_KEY);
+        return applicationUrl + determineUrlNameForClass(documentClass) + transformClassName(documentClass) + ".do?methodToCall=docHandler&command=initiate&docTypeName="+documentTypeName;
+    }
+
+    protected String transformClassName(Class<? extends Document> documentClass) {
+        return documentClass.getSimpleName().replace("Document", "");
+    }
+
+    protected String determineUrlNameForClass(Class<? extends Document> documentClass) {
+        final ModuleService module = getKualiModuleService().getResponsibleModuleService(documentClass);
+        if (!ObjectUtils.isNull(module)) {
+            final ModuleConfiguration moduleConfiguration = module.getModuleConfiguration();
+            return lookupUrlNameFromNamespace(moduleConfiguration.getNamespaceCode());
+        }
+        return "";
+    }
+
+    protected String lookupUrlNameFromNamespace(String namespaceCode) {
+        return getNamespaceCodeToUrlName().get(namespaceCode);
+    }
+
+    protected Map<String, String> constructLinkInfo(String label, String link) {
         Map<String, String> linkInfo = new ConcurrentHashMap<>();
         linkInfo.put("label", label);
+        linkInfo.put("link", link);
         return linkInfo;
+    }
+
+    public ConfigurationService getConfigurationService() {
+        return configurationService;
+    }
+
+    public void setConfigurationService(ConfigurationService configurationService) {
+        this.configurationService = configurationService;
     }
 
     public DocumentDictionaryService getDocumentDictionaryService() {
@@ -69,11 +139,27 @@ public class PreferencesServiceImpl implements PreferencesService {
         this.documentDictionaryService = documentDictionaryService;
     }
 
+    public KualiModuleService getKualiModuleService() {
+        return kualiModuleService;
+    }
+
+    public void setKualiModuleService(KualiModuleService kualiModuleService) {
+        this.kualiModuleService = kualiModuleService;
+    }
+
     public PreferencesDao getPreferencesDao() {
         return preferencesDao;
     }
 
     public void setPreferencesDao(PreferencesDao preferencesDao) {
         this.preferencesDao = preferencesDao;
+    }
+
+    public Map<String, String> getNamespaceCodeToUrlName() {
+        return namespaceCodeToUrlName;
+    }
+
+    public void setNamespaceCodeToUrlName(Map<String, String> namespaceCodeToUrlName) {
+        this.namespaceCodeToUrlName = namespaceCodeToUrlName;
     }
 }
--- kfs-core/src/test/java/org/kuali/kfs/sys/service/impl/PreferencesServiceImplTest.java
+++ kfs-core/src/test/java/org/kuali/kfs/sys/service/impl/PreferencesServiceImplTest.java
@@ -3,12 +3,31 @@ package org.kuali.kfs.sys.service.impl;
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang.StringUtils;
 import org.junit.Test;
+import org.kuali.kfs.fp.document.ServiceBillingDocument;
+import org.kuali.kfs.sys.FinancialSystemModuleConfiguration;
+import org.kuali.kfs.sys.KFSConstants;
 import org.kuali.kfs.sys.dataaccess.PreferencesDao;
 import org.junit.Assert;
+import org.kuali.rice.core.api.config.property.ConfigurationService;
+import org.kuali.rice.krad.bo.BusinessObject;
+import org.kuali.rice.krad.bo.ExternalizableBusinessObject;
+import org.kuali.rice.krad.bo.ModuleConfiguration;
+import org.kuali.rice.krad.datadictionary.BusinessObjectEntry;
+import org.kuali.rice.krad.datadictionary.DocumentEntry;
+import org.kuali.rice.krad.datadictionary.MaintenanceDocumentEntry;
+import org.kuali.rice.krad.document.Document;
+import org.kuali.rice.krad.document.DocumentAuthorizer;
+import org.kuali.rice.krad.document.DocumentPresentationController;
+import org.kuali.rice.krad.maintenance.Maintainable;
+import org.kuali.rice.krad.maintenance.MaintenanceDocument;
+import org.kuali.rice.krad.rules.rule.BusinessRule;
+import org.kuali.rice.krad.service.DocumentDictionaryService;
+import org.kuali.rice.krad.service.KualiModuleService;
+import org.kuali.rice.krad.service.ModuleService;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class PreferencesServiceImplTest {
@@ -42,7 +61,7 @@ public class PreferencesServiceImplTest {
                 ip.put("institutionName", "Monsters");
 
                 Map<String, String> link = new ConcurrentHashMap<>();
-                link.put("documentTypeCode", "REQS");
+                link.put("documentTypeCode", "SB");
 
                 List<Map<String, String>> links = new ArrayList<>();
                 links.add(link);
@@ -59,7 +78,12 @@ public class PreferencesServiceImplTest {
                 return ip;
             }
         });
+        preferencesServiceImpl.setDocumentDictionaryService(new StubDocumentDictionaryService());
+        preferencesServiceImpl.setConfigurationService(new StubConfigurationService());
+        preferencesServiceImpl.setKualiModuleService(new StubKualiModuleService());
+
         Map<String, Object> preferences = preferencesServiceImpl.findInstitutionPreferences();
+
         Assert.assertNotNull("Preferences should really really exist", preferences);
         Assert.assertNotNull("Link Groups should exist", preferences.get("linkGroups"));
         Assert.assertTrue("Link Groups should be a List", (preferences.get("linkGroups") instanceof List));
@@ -80,7 +104,7 @@ public class PreferencesServiceImplTest {
                 ip.put("institutionName", "Monsters");
 
                 Map<String, String> link = new ConcurrentHashMap<>();
-                link.put("documentTypeCode", "REQS");
+                link.put("documentTypeCode", "SB");
 
                 List<Map<String, String>> links = new ArrayList<>();
                 links.add(link);
@@ -97,7 +121,12 @@ public class PreferencesServiceImplTest {
                 return ip;
             }
         });
+        preferencesServiceImpl.setDocumentDictionaryService(new StubDocumentDictionaryService());
+        preferencesServiceImpl.setConfigurationService(new StubConfigurationService());
+        preferencesServiceImpl.setKualiModuleService(new StubKualiModuleService());
+
         Map<String, Object> preferences = preferencesServiceImpl.findInstitutionPreferences();
+
         Assert.assertNotNull("Preferences should really really exist", preferences);
         Assert.assertNotNull("Link Groups should exist", preferences.get("linkGroups"));
         Assert.assertTrue("Link Groups should be a List", (preferences.get("linkGroups") instanceof List));
@@ -108,5 +137,337 @@ public class PreferencesServiceImplTest {
         Assert.assertTrue("Link should have a label", !StringUtils.isBlank(((List<Map<String, String>>) ((List<Map<String, Object>>) preferences.get("linkGroups")).get(0).get("links")).get(0).get("label")));
         Assert.assertTrue("Link should have a link", !StringUtils.isBlank(((List<Map<String, String>>)((List<Map<String, Object>>)preferences.get("linkGroups")).get(0).get("links")).get(0).get("link")));
         Assert.assertTrue("Link should NOT have a document type", StringUtils.isBlank(((List<Map<String, String>>)((List<Map<String, Object>>)preferences.get("linkGroups")).get(0).get("links")).get(0).get("documentTypeCode")));
+    }
+
+    protected class StubDocumentDictionaryService implements DocumentDictionaryService {
+        @Override
+        public String getLabel(String documentTypeName) {
+            if (StringUtils.equals(documentTypeName, "SB")) {
+                return "Service Billing";
+            }
+            return null;
+        }
+
+        @Override
+        public String getMaintenanceDocumentTypeName(Class dataObjectClass) {
+            return null;
+        }
+
+        @Override
+        public String getDescription(String documentTypeName) {
+            return null;
+        }
+
+        @Override
+        public Collection getDefaultExistenceChecks(Class dataObjectClass) {
+            return null;
+        }
+
+        @Override
+        public Collection getDefaultExistenceChecks(Document document) {
+            return null;
+        }
+
+        @Override
+        public Collection getDefaultExistenceChecks(String docTypeName) {
+            return null;
+        }
+
+        @Override
+        public Class<?> getMaintenanceDataObjectClass(String docTypeName) {
+            return null;
+        }
+
+        @Override
+        public Class<? extends Maintainable> getMaintainableClass(String docTypeName) {
+            return null;
+        }
+
+        @Override
+        public Class<? extends BusinessRule> getBusinessRulesClass(Document document) {
+            return null;
+        }
+
+        @Override
+        public Boolean getAllowsCopy(Document document) {
+            return null;
+        }
+
+        @Override
+        public Boolean getAllowsNewOrCopy(String docTypeName) {
+            return null;
+        }
+
+        @Override
+        public DocumentEntry getDocumentEntry(String docTypeName) {
+            return null;
+        }
+
+        @Override
+        public DocumentEntry getDocumentEntryByClass(Class<? extends Document> documentClass) {
+            return null;
+        }
+
+        @Override
+        public MaintenanceDocumentEntry getMaintenanceDocumentEntry(String docTypeName) {
+            return null;
+        }
+
+        @Override
+        public Class<?> getDocumentClassByName(String documentTypeName) {
+            if (StringUtils.equals(documentTypeName, "SB")) {
+                return ServiceBillingDocument.class;
+            }
+            return null;
+        }
+
+        @Override
+        public String getDocumentTypeByClass(Class<? extends Document> documentClass) {
+            return null;
+        }
+
+        @Override
+        public Boolean getAllowsRecordDeletion(Class dataObjectClass) {
+            return null;
+        }
+
+        @Override
+        public Boolean getAllowsRecordDeletion(MaintenanceDocument document) {
+            return null;
+        }
+
+        @Override
+        public List<String> getLockingKeys(String docTypeName) {
+            return null;
+        }
+
+        @Override
+        public boolean getPreserveLockingKeysOnCopy(Class dataObjectClass) {
+            return false;
+        }
+
+        @Override
+        public DocumentAuthorizer getDocumentAuthorizer(String documentType) {
+            return null;
+        }
+
+        @Override
+        public DocumentAuthorizer getDocumentAuthorizer(Document document) {
+            return null;
+        }
+
+        @Override
+        public DocumentPresentationController getDocumentPresentationController(String documentType) {
+            return null;
+        }
+
+        @Override
+        public DocumentPresentationController getDocumentPresentationController(Document document) {
+            return null;
+        }
+    }
+
+    protected class StubConfigurationService implements ConfigurationService {
+        @Override
+        public String getPropertyValueAsString(String s) {
+            if (StringUtils.equals(s, KFSConstants.APPLICATION_URL_KEY)) {
+                return "http://tst.kfs.kuali.org/kfs-tst/";
+            }
+            return null;
+        }
+
+        @Override
+        public boolean getPropertyValueAsBoolean(String s) {
+            return false;
+        }
+
+        @Override
+        public Map<String, String> getAllProperties() {
+            return null;
+        }
+    }
+
+    protected class StubKualiModuleService implements KualiModuleService {
+        @Override
+        public List<ModuleService> getInstalledModuleServices() {
+            return null;
+        }
+
+        @Override
+        public ModuleService getModuleService(String moduleId) {
+            return null;
+        }
+
+        @Override
+        public ModuleService getModuleServiceByNamespaceCode(String namespaceCode) {
+            return null;
+        }
+
+        @Override
+        public boolean isModuleServiceInstalled(String namespaceCode) {
+            return false;
+        }
+
+        @Override
+        public ModuleService getResponsibleModuleService(Class boClass) {
+            if (boClass.equals(ServiceBillingDocument.class)) {
+                ModuleService fpModule = new ModuleService() {
+                    @Override
+                    public ModuleConfiguration getModuleConfiguration() {
+                        FinancialSystemModuleConfiguration moduleConfig = new FinancialSystemModuleConfiguration();
+                        moduleConfig.setNamespaceCode("KFS-FP");
+                        return moduleConfig;
+                    }
+
+                    @Override
+                    public boolean isResponsibleFor(Class businessObjectClass) {
+                        return false;
+                    }
+
+                    @Override
+                    public boolean isResponsibleForJob(String jobName) {
+                        return false;
+                    }
+
+                    @Override
+                    public List listPrimaryKeyFieldNames(Class businessObjectInterfaceClass) {
+                        return null;
+                    }
+
+                    @Override
+                    public List<List<String>> listAlternatePrimaryKeyFieldNames(Class businessObjectInterfaceClass) {
+                        return null;
+                    }
+
+                    @Override
+                    public BusinessObjectEntry getExternalizableBusinessObjectDictionaryEntry(Class businessObjectInterfaceClass) {
+                        return null;
+                    }
+
+                    @Override
+                    public <T extends ExternalizableBusinessObject> T getExternalizableBusinessObject(Class<T> businessObjectClass, Map<String, Object> fieldValues) {
+                        return null;
+                    }
+
+                    @Override
+                    public <T extends ExternalizableBusinessObject> List<T> getExternalizableBusinessObjectsList(Class<T> businessObjectClass, Map<String, Object> fieldValues) {
+                        return null;
+                    }
+
+                    @Override
+                    public <T extends ExternalizableBusinessObject> List<T> getExternalizableBusinessObjectsListForLookup(Class<T> businessObjectClass, Map<String, Object> fieldValues, boolean unbounded) {
+                        return null;
+                    }
+
+                    @Override
+                    public String getExternalizableDataObjectInquiryUrl(Class<?> inquiryDataObjectClass, Properties parameters) {
+                        return null;
+                    }
+
+                    @Override
+                    public String getExternalizableDataObjectLookupUrl(Class<?> inquiryDataObjectClass, Properties parameters) {
+                        return null;
+                    }
+
+                    @Override
+                    public String getExternalizableBusinessObjectInquiryUrl(Class inquiryBusinessObjectClass, Map<String, String[]> parameters) {
+                        return null;
+                    }
+
+                    @Override
+                    public String getExternalizableBusinessObjectLookupUrl(Class inquiryBusinessObjectClass, Map<String, String> parameters) {
+                        return null;
+                    }
+
+                    @Override
+                    public <T extends ExternalizableBusinessObject> T retrieveExternalizableBusinessObjectIfNecessary(BusinessObject businessObject, T currentInstanceExternalizableBO, String externalizableRelationshipName) {
+                        return null;
+                    }
+
+                    @Override
+                    public <T extends ExternalizableBusinessObject> List<T> retrieveExternalizableBusinessObjectsList(BusinessObject businessObject, String externalizableRelationshipName, Class<T> externalizableClazz) {
+                        return null;
+                    }
+
+                    @Override
+                    public boolean isExternalizable(Class boClass) {
+                        return false;
+                    }
+
+                    @Override
+                    public boolean isExternalizableBusinessObjectLookupable(Class boClass) {
+                        return false;
+                    }
+
+                    @Override
+                    public boolean isExternalizableBusinessObjectInquirable(Class boClass) {
+                        return false;
+                    }
+
+                    @Override
+                    public <T extends ExternalizableBusinessObject> T createNewObjectFromExternalizableClass(Class<T> boClass) {
+                        return null;
+                    }
+
+                    @Override
+                    public <E extends ExternalizableBusinessObject> Class<E> getExternalizableBusinessObjectImplementation(Class<E> externalizableBusinessObjectInterface) {
+                        return null;
+                    }
+
+                    @Override
+                    public boolean isLocked() {
+                        return false;
+                    }
+
+                    @Override
+                    public boolean goToCentralRiceForInquiry() {
+                        return false;
+                    }
+
+                    @Override
+                    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+
+                    }
+
+                    @Override
+                    public void afterPropertiesSet() throws Exception {
+
+                    }
+                };
+                return fpModule;
+
+            }
+            return null;
+        }
+
+        @Override
+        public ModuleService getResponsibleModuleServiceForJob(String jobName) {
+            return null;
+        }
+
+        @Override
+        public void setInstalledModuleServices(List<ModuleService> moduleServices) {
+
+        }
+
+        @Override
+        public List<String> getDataDictionaryPackages() {
+            return null;
+        }
+
+        @Override
+        public String getNamespaceName(String namespaceCode) {
+            return null;
+        }
+
+        @Override
+        public String getNamespaceCode(Class<?> documentOrStepClass) {
+            return null;
+        }
+
+        @Override
+        public String getComponentCode(Class<?> documentOrStepClass) {
+            return null;
+        }
     }
 }
