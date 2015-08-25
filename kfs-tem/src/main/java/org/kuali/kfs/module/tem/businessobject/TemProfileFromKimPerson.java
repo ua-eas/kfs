0c0081ee216ef864f2136cdf34bccf3db0874dfb
kkronenb <kevin@kuali.co>
2015-08-25 14:32:11 -0600

    more work

--- kfs-tem/src/main/java/org/kuali/kfs/module/tem/businessobject/TemProfileFromKimPerson.java
+++ kfs-tem/src/main/java/org/kuali/kfs/module/tem/businessobject/TemProfileFromKimPerson.java
@@ -44,10 +44,9 @@ public class TemProfileFromKimPerson extends PersistableBusinessObjectBase imple
     private String emailAddress;
     private String phoneNumber;
     private String employeeStatusCode;
-    //TODO: Spaghetti fix
-   // protected EntityEmploymentStatusEbo employeeStatus;
+    protected EntityEmploymentStatusEbo employeeStatus;
     protected String employeeTypeCode;
-   // protected EntityEmploymentTypeEbo employeeType;
+    protected EntityEmploymentTypeEbo employeeType;
     protected String primaryDepartmentCode;
     protected String campusCode;
     protected CampusEbo campus;
@@ -248,20 +247,20 @@ public class TemProfileFromKimPerson extends PersistableBusinessObjectBase imple
      * Gets the employeeStatus attribute.
      * @return Returns the employeeStatus.
      */
-//    @ManyToOne
-//    @JoinColumn(name = "emp_status_cd")
-//    public EntityEmploymentStatusEbo getEmployeeStatus() {
-//        return employeeStatus;
-//    }
-//
-//
-//    /**
-//     * Sets the employeeStatus attribute value.
-//     * @param employeeStatus The employeeStatus to set.
-//     */
-//    public void setEmployeeStatus(EntityEmploymentStatusBo employeeStatus) {
-//        this.employeeStatus = employeeStatus;
-//    }
+    @ManyToOne
+    @JoinColumn(name = "emp_status_cd")
+    public EntityEmploymentStatusEbo getEmployeeStatus() {
+        return employeeStatus;
+    }
+
+
+    /**
+     * Sets the employeeStatus attribute value.
+     * @param employeeStatus The employeeStatus to set.
+     */
+    public void setEmployeeStatus(EntityEmploymentStatusBo employeeStatus) {
+        this.employeeStatus = employeeStatus;
+    }
 
 
     /**
@@ -283,24 +282,24 @@ public class TemProfileFromKimPerson extends PersistableBusinessObjectBase imple
     }
 
 
-//    /**
-//     * Gets the employeeType attribute.
-//     * @return Returns the employeeType.
-//     */
-//    @ManyToOne
-//    @JoinColumn(name = "emp_typ_cd")
-//    public EntityEmploymentTypeEbo getEmployeeType() {
-//        return employeeType;
-//    }
-//
-//
-//    /**
-//     * Sets the employeeType attribute value.
-//     * @param employeeType The employeeType to set.
-//     */
-//    public void setEmployeeType(EntityEmploymentTypeEbo employeeType) {
-//        this.employeeType = employeeType;
-//    }
+    /**
+     * Gets the employeeType attribute.
+     * @return Returns the employeeType.
+     */
+    @ManyToOne
+    @JoinColumn(name = "emp_typ_cd")
+    public EntityEmploymentTypeEbo getEmployeeType() {
+        return employeeType;
+    }
+
+
+    /**
+     * Sets the employeeType attribute value.
+     * @param employeeType The employeeType to set.
+     */
+    public void setEmployeeType(EntityEmploymentTypeEbo employeeType) {
+        this.employeeType = employeeType;
+    }
 
 
     /**
