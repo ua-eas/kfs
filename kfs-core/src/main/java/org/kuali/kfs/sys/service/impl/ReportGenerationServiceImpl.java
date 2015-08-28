baf6efe354fa19964f415681cd783c977966e1c0
Bryan Hutchinson <bryan@kuali.co>
2015-08-28 13:28:02 -0400

    KFSTI-2516 avoid NPE

--- kfs-core/src/main/java/org/kuali/kfs/sys/service/impl/ReportGenerationServiceImpl.java
+++ kfs-core/src/main/java/org/kuali/kfs/sys/service/impl/ReportGenerationServiceImpl.java
@@ -102,7 +102,9 @@ public class ReportGenerationServiceImpl implements ReportGenerationService {
     protected Map<String, Object> decorateReportData(Map<String, Object> reportData) {
         Map<String, Object> decoratedReportData = new ConcurrentHashMap<>();
         decoratedReportData.putAll(reportData);
-        decoratedReportData.put(ReportGeneration.PARAMETER_NAME_SUBREPORT_DIR, new File(System.getProperty("java.io.tmpdir").concat(File.separator).concat(reportData.get(ReportGeneration.PARAMETER_NAME_SUBREPORT_DIR).toString())).getAbsolutePath().concat(File.separator));
+        if (reportData.containsKey(ReportGeneration.PARAMETER_NAME_SUBREPORT_DIR)) {
+            decoratedReportData.put(ReportGeneration.PARAMETER_NAME_SUBREPORT_DIR, new File(System.getProperty("java.io.tmpdir").concat(File.separator).concat(reportData.get(ReportGeneration.PARAMETER_NAME_SUBREPORT_DIR).toString())).getAbsolutePath().concat(File.separator));
+        }
         return decoratedReportData;
     }
 
