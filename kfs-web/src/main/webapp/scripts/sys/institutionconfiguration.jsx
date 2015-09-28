0b0dd16a80d06bac08294feb361983ccd515a8c6
smith750 <jksmith@kuali.co>
2015-09-28 18:36:53 -0400

    move to an immutable view model

--- kfs-web/src/main/webapp/scripts/sys/institutionconfiguration.jsx
+++ kfs-web/src/main/webapp/scripts/sys/institutionconfiguration.jsx
@@ -1,9 +1,10 @@
 import KfsUtils from './utils.js';
 import _ from 'lodash';
+import Immutable from 'immutable';
 
 let InstitutionConfig = React.createClass({
     getInitialState() {
-        return {linkGroups: []};
+        return {linkGroups: new Immutable.List()};
     },
     componentWillMount() {
         let linkGroupPath = KfsUtils.getUrlPathPrefix() + "sys/preferences/config/groups";
@@ -12,7 +13,7 @@ let InstitutionConfig = React.createClass({
             dataType: 'json',
             type: 'GET',
             success: function(linkGroups) {
-                this.setState({linkGroups: linkGroups});
+                this.setState({linkGroups: new Immutable.List(linkGroups)});
             }.bind(this),
             error: function(xhr, status, err) {
                 console.error(status, err.toString());
@@ -20,7 +21,7 @@ let InstitutionConfig = React.createClass({
         });
     },
     updateLinkGroups(linkGroups) {
-        this.setState({linkGroups: linkGroups});
+        this.setState({linkGroups: new Immutable.List(linkGroups)});
 
     },
     render() {
@@ -33,6 +34,30 @@ let InstitutionConfig = React.createClass({
 });
 
 let LinkGroups = React.createClass({
+    removeElementAt(list, removeIndex) {
+        if (removeIndex === 0) {
+            return list.slice(1);
+        } else if (list.size === (removeIndex + 1)) {
+            return list.slice(0, -1);
+        } else {
+            return list.slice(0, removeIndex - 1).concat(list.slice(removeIndex+1));
+        }
+    },
+    insertElementAt(list, ele, addIndex) {
+        let eleList = Immutable.List.of(ele);
+        if (addIndex === 0) {
+            return eleList.concat(list);
+        } else if (list.size === (addIndex + 1)) {
+            return list.concat(eleList);
+        } else {
+            return list.slice(0, addIndex).concat(eleList).concat(list.slice(addIndex));
+        }
+    },
+    moveElement(list, fromIndex, toIndex) {
+        let movingElement = list.get(fromIndex)
+        let updatedList = this.insertElementAt(this.removeElementAt(list, fromIndex), movingElement, toIndex);
+        return updatedList;
+    },
     componentDidMount() {
         let self = this;
 
@@ -44,13 +69,12 @@ let LinkGroups = React.createClass({
                 },
                 update: function (event, ui) {
                     event.stopPropagation();
-                    var startIndex = ui.item.data("startindex");
-                    var newIndex = ui.item.index();
-                    let linkGroups = _.cloneDeep(self.props.linkGroups);
+                    let startIndex = ui.item.data("startindex");
+                    let newIndex = ui.item.index();
                     if (newIndex != startIndex) {
-                        linkGroups.splice(newIndex, 0, linkGroups.splice(startIndex, 1)[0] );
+                        let updatedLinkGroups = self.moveElement(self.props.linkGroups, startIndex, newIndex);
                         $("#sortable").sortable('cancel');
-                        self.props.updateLinkGroups(linkGroups);
+                        self.props.updateLinkGroups(updatedLinkGroups);
                     }
                 }
             });
