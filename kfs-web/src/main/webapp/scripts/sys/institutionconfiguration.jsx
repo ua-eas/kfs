805ae9bfdda8202d3cced3667258a4e0029b64eb
tfoxley <travisfoxley@gmail.com>
2015-09-28 15:44:52 -0600

    hackweek - get sortable list for link groups working

--- kfs-web/src/main/webapp/scripts/sys/institutionconfiguration.jsx
+++ kfs-web/src/main/webapp/scripts/sys/institutionconfiguration.jsx
@@ -1,8 +1,9 @@
 import KfsUtils from './utils.js';
+import _ from 'lodash';
 
 let InstitutionConfig = React.createClass({
     getInitialState() {
-        return {linkGroupModel: []};
+        return {linkGroups: []};
     },
     componentWillMount() {
         let linkGroupPath = KfsUtils.getUrlPathPrefix() + "sys/preferences/config/groups";
@@ -11,20 +12,21 @@ let InstitutionConfig = React.createClass({
             dataType: 'json',
             type: 'GET',
             success: function(linkGroups) {
-                this.setState({linkGroupModel: linkGroups});
+                this.setState({linkGroups: linkGroups});
             }.bind(this),
             error: function(xhr, status, err) {
                 console.error(status, err.toString());
             }.bind(this)
         });
     },
-    updateLinkGroupModel(currentLinkGroupModel) {
-        this.setState({linkGroupModel: currentLinkGroupModel});
+    updateLinkGroups(linkGroups) {
+        this.setState({linkGroups: linkGroups});
+
     },
     render() {
         return (
             <div className="instconfig">
-                <LinkGroups linkGroups={this.state.linkGroupModel} updateLinkGroupModel={this.updateLinkGroupModel}/>
+                <LinkGroups linkGroups={this.state.linkGroups} updateLinkGroups={this.updateLinkGroups}/>
             </div>
         )
     }
@@ -42,13 +44,13 @@ let LinkGroups = React.createClass({
                 },
                 update: function (event, ui) {
                     event.stopPropagation();
-                    console.log("doing jQuery update");
                     var startIndex = ui.item.data("startindex");
                     var newIndex = ui.item.index();
-                    let currentLinkGroupModel = self.props.linkGroups;
+                    let linkGroups = _.cloneDeep(self.props.linkGroups);
                     if (newIndex != startIndex) {
-                        currentLinkGroupModel.splice(newIndex, 0, currentLinkGroupModel.splice(startIndex, 1)[0] );
-                        self.props.updateLinkGroupModel(currentLinkGroupModel);
+                        linkGroups.splice(newIndex, 0, linkGroups.splice(startIndex, 1)[0] );
+                        $("#sortable").sortable('cancel');
+                        self.props.updateLinkGroups(linkGroups);
                     }
                 }
             });
@@ -56,7 +58,6 @@ let LinkGroups = React.createClass({
         }
     },
     render() {
-        console.log("doing react render - LinkGroups");
         let linkGroupElements = this.props.linkGroups.map((linkGroup) => {
             return <LinkGroup linkGroup={linkGroup} key={KfsUtils.buildKeyFromLabel(linkGroup.label)}/>
         });
@@ -66,7 +67,7 @@ let LinkGroups = React.createClass({
 
 let LinkGroup = React.createClass({
     render() {
-        return <li class="">{this.props.linkGroup.label}</li>
+        return <li className="linkgroup">{this.props.linkGroup.label}</li>
     }
 });
 
