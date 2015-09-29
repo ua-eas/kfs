b25dc2c81f14b447059a13acdc4b39271266d309
tfoxley <travisfoxley@gmail.com>
2015-09-29 12:31:36 -0600

    hackweek - start making sublinks sortable

--- kfs-web/src/main/webapp/scripts/sys/institutionconfiguration.jsx
+++ kfs-web/src/main/webapp/scripts/sys/institutionconfiguration.jsx
@@ -14,7 +14,7 @@ let InstitutionConfig = React.createClass({
             dataType: 'json',
             type: 'GET',
             success: function(linkGroups) {
-                this.setState({linkGroups: new Immutable.List(linkGroups)});
+                this.setState({linkGroups: Immutable.fromJS(linkGroups)});
             }.bind(this),
             error: function(xhr, status, err) {
                 console.error(status, err.toString());
@@ -39,6 +39,8 @@ let InstitutionConfig = React.createClass({
                             updateLinkGroups={this.updateLinkGroups}
                             toggleLinkGroup={this.toggleLinkGroup}
                             expandedLinkGroup={this.state.expandedLinkGroup}/>
+
+                <LinkGroupLinks linkGroups={this.state.linkGroups} expandedLinkGroup={this.state.expandedLinkGroup} />
             </div>
         )
     }
@@ -89,36 +91,67 @@ let LinkGroups = React.createClass({
 
 let SubLinkGroup = React.createClass({
     render() {
+        let divClassName = 'admin-sublinks';
+        if (this.props.groupLabel !== this.props.expandedLinkGroup) {
+            divClassName += ' collapse';
+        }
         return (
-            <div id={this.props.id + "-menu"} className='admin-sublinks collapse'>
-                <SubLinkType links={this.props.links} type='activities'/>
-                <SubLinkType links={this.props.links} type='reference'/>
-                <SubLinkType links={this.props.links} type='administration'/>
+            <div id={this.props.id + "-menu"} className={divClassName}>
+                <SubLinkType groupLabel={this.props.groupLabel} links={this.props.links} type='activities'/>
+                <SubLinkType groupLabel={this.props.groupLabel} links={this.props.links} type='reference'/>
+                <SubLinkType groupLabel={this.props.groupLabel} links={this.props.links} type='administration'/>
             </div>
         )
     }
 });
 
 let SubLinkType = React.createClass({
-   render() {
-       let linkList = "";
-       let self = this;
-       if (this.props.links) {
-           let linkElements = this.props.links.filter((link) => {
-               return link.type === self.props.type;
-           }).map((link, i) => {
-               return <li><Link key={self.props.type + "_" + i} url={link.link} label={link.label}
-                                className="list-group-item"/></li>;
-           });
-           linkList = <ul>{linkElements}</ul>;
-       }
+    updateSublinkTypeLinks() {
+        console.log('updating sublinks');
+    },
+    render() {
        return (
            <div>
                <h4 key={this.props.type + "Label"}>{this.props.type}</h4>
-               {linkList}
+               <SubLinkTypeLinks links={this.props.links}
+                                 type={this.props.type}
+                                 groupLabel={this.props.groupLabel}
+                                 updateSublinkTypeLinks={this.updateSublinkTypeLinks}/>
            </div>
        )
    }
+});
+
+let SubLinkTypeLinks = React.createClass({
+    componentDidMount() {
+        let self = this;
+        let id = "sortable-" + buildKeyFromLabel(this.props.groupLabel) + "-" + this.props.type;
+        buildSortableDropHandler(id, self, 'links', 'updateSublinkTypeLinks');
+    },
+    render() {
+        let linkList = "";
+        let self = this;
+        if (this.props.links) {
+            let linkElements = this.props.links.filter((link) => {
+                return link.type === self.props.type;
+            }).map((link) => {
+                return <li><span className="list-group-item">{link.label}</span></li>;
+            });
+            let id = "sortable-" + buildKeyFromLabel(this.props.groupLabel) + "-" + this.props.type;
+            linkList = <ul id={id}>{linkElements}</ul>;
+        }
+        return linkList
+    }
+});
+
+let LinkGroupLinks = React.createClass({
+    render() {
+        let linkGroupLinkElements = this.props.linkGroups.map((linkGroup) => {
+            let id = buildKeyFromLabel(linkGroup.label)
+            return <SubLinkGroup id={id} links={linkGroup.links} groupLabel={linkGroup.label} expandedLinkGroup={this.props.expandedLinkGroup} />
+        });
+        return <div id="linkGroupLinksList">{linkGroupLinkElements}</div>;
+    }
 });
 
 var determinePanelClassName = function(expandedLinkGroup, label) {
@@ -132,7 +165,7 @@ var determinePanelClassName = function(expandedLinkGroup, label) {
 let LinkGroup = React.createClass({
     render() {
         let label = this.props.linkGroup.label
-        let id = buildKeyFromLabel(label)
+
 
         let panelClassName = determinePanelClassName(this.props.expandedLinkGroup, label)
 
@@ -142,7 +175,6 @@ let LinkGroup = React.createClass({
                     <span className="move"></span>
                     <span>{this.props.linkGroup.label}</span>
                 </a>
-                <SubLinkGroup id={id} links={this.props.linkGroup.links} />
             </li>
         )
     }
