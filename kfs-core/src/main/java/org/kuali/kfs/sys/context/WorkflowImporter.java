5ff095f4a9bbf70001fdf71003b0ba0c3f87ca51
kkronenb <kevin@kuali.co>
2015-10-08 08:52:20 -0400

    FINP-5 get this working via command line

--- kfs-core/src/main/java/org/kuali/kfs/sys/context/WorkflowImporter.java
+++ kfs-core/src/main/java/org/kuali/kfs/sys/context/WorkflowImporter.java
@@ -18,44 +18,29 @@
  */
 package org.kuali.kfs.sys.context;
 
-import java.applet.AppletContext;
-import java.io.File;
-import java.io.FileFilter;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.nio.channels.FileChannel;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Properties;
-
-import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang.StringUtils;
-import org.apache.log4j.Level;
-import org.apache.log4j.Logger;
 import org.kuali.kfs.krad.datadictionary.DataDictionaryException;
 import org.kuali.kfs.krad.service.KRADServiceLocator;
 import org.kuali.kfs.krad.util.ResourceLoaderUtil;
-import org.kuali.rice.core.api.CoreApiServiceLocator;
 import org.kuali.rice.core.api.config.property.ConfigContext;
-import org.kuali.rice.core.api.impex.xml.DirectoryXmlDocCollection;
-import org.kuali.rice.core.api.impex.xml.FileXmlDocCollection;
-import org.kuali.rice.core.api.impex.xml.XmlDocCollection;
-import org.kuali.rice.core.api.impex.xml.XmlIngesterService;
-import org.kuali.rice.core.api.impex.xml.ZipXmlDocCollection;
 import org.kuali.rice.core.impl.config.property.JAXBConfigImpl;
-import org.kuali.rice.kew.batch.XmlPollerServiceImpl;
+import org.kuali.rice.kew.doctype.service.DocumentTypeService;
+import org.kuali.rice.kew.service.KEWServiceLocator;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.ResourcePatternUtils;
 
-public class WorkflowImporter {
-    private static final String WORKFLOW_DIRECTORY = "workflow.directory";
-    private static final org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(WorkflowImporter.class);
-    private static final String PENDING_MOVE_FAILED_ARCHIVE_FILE = "movesfailed";
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Properties;
 
+public class WorkflowImporter {
+    private static final org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(WorkflowImporter.class);
+
+    private static final String WORKFLOW_DIRECTORY = "workflow.directory";
     private static ClassPathXmlApplicationContext context;
 
     public static void main(String[] args) {
@@ -72,7 +57,6 @@ public class WorkflowImporter {
         LOG.info("Initializing Kuali Rice Application...");
 
         String bootstrapSpringBeans = "kfs-startup.xml";
-//        String bootstrapSpringBeans = "kfs-workflow-importer-startup.xml";
 
         Properties baseProps = new Properties();
         baseProps.putAll(System.getProperties());
@@ -97,18 +81,18 @@ public class WorkflowImporter {
     }
 
     protected static void importWorkflow(ApplicationContext applicationContext, String xmlDir) {
-        List<XmlDocCollection> collections = new ArrayList<>();
+        DocumentTypeService documentTypeService = KEWServiceLocator.getDocumentTypeService();
 
         for (String file : Arrays.asList(xmlDir.split(","))) {
             String trimmedFile = file.trim();
             if (!trimmedFile.isEmpty()) {
                 if (trimmedFile.endsWith(".xml")) {
-                    try {
+                   try {
                         for (Resource resource : ResourcePatternUtils.getResourcePatternResolver(applicationContext).getResources(trimmedFile)) {
                             if (resource.exists()) {
                                 final String resourcePath = ResourceLoaderUtil.parseResourcePathFromUrl(resource);
                                 if (!StringUtils.isBlank(resourcePath)) {
-                                    collections.add(new FileXmlDocCollection(resource.getFile()));
+                                    documentTypeService.loadXml(resource.getInputStream(), "KFS");
                                 }
                             } else {
                                 LOG.warn("Could not find " + xmlDir);
@@ -117,24 +101,16 @@ public class WorkflowImporter {
                         }
                     } catch (IOException e) {
                         LOG.error("Error loading resource: " + trimmedFile);
+                        e.printStackTrace();
                     }
                 } else if (trimmedFile.endsWith(".zip")) {
                     try {
-                        collections.add(new ZipXmlDocCollection(new File(trimmedFile)));
+                        documentTypeService.loadXml(new FileInputStream(new File(trimmedFile)), "KFS");
                     } catch (IOException ioe) {
                         LOG.error("Unable to load file: " + trimmedFile);
                     }
                 }
             }
-        }
-
-        LOG.info("Found " + collections.size() + " files to ingest.");
-
-        XmlIngesterService xmlIngesterService = CoreApiServiceLocator.getXmlIngesterService();
-        try {
-            xmlIngesterService.ingest(collections);
-        } catch (Exception e) {
-            LOG.error("Well something went wrong, hopefully there are some error messages", e);
         }
     }
 }
