e1e82b6d7208ec5b5c1ab46ac1e4dc6acde4c2ed
Bryan Hutchinson <bryan@kuali.co>
2015-11-06 10:45:38 -0500

    FINI-207 - fix cg liquibase

---
+++ kfs-cg/src/test/java/org/kuali/kfs/module/cg/database/CgLiquibaseModifySqlTest.java
@@ -0,0 +1,29 @@
+/*
+ * The Kuali Financial System, a comprehensive financial management system for higher education.
+ *
+ * Copyright 2005-2014 The Kuali Foundation
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.kuali.kfs.module.cg.database;
+
+import org.junit.Test;
+import org.kuali.kfs.sys.database.LiquibaseTestBase;
+
+public class CgLiquibaseModifySqlTest extends LiquibaseTestBase {
+    @Test
+    public void testCg() throws Exception {
+        testForMissingModifySql("/org/kuali/kfs/module/cg/db/phase1/kfs-cg_createTable.xml");
+    }
+}
