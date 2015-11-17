2073a98eb5a0b9769544566e1f7a2bb55a8d76bf
smith750 <jksmith@kuali.co>
2015-11-17 14:56:06 -0500

    FINI-233

--- kfs-ar/src/main/java/org/kuali/kfs/module/ar/document/web/struts/CustomerCreditMemoAction.java
+++ kfs-ar/src/main/java/org/kuali/kfs/module/ar/document/web/struts/CustomerCreditMemoAction.java
@@ -313,7 +313,7 @@ public class CustomerCreditMemoAction extends KualiTransactionalDocumentActionBa
         Properties parameters = new Properties();
         parameters.put(KFSConstants.DISPATCH_REQUEST_PARAMETER, methodToCall);
         parameters.put(KFSConstants.PARAMETER_DOC_ID, docId);
-        parameters.put(KFSConstants.PARAMETER_COMMAND, KewApiConstants.ACTIONLIST_COMMAND);
+        parameters.put(KFSConstants.PARAMETER_COMMAND, KewApiConstants.DOCSEARCH_COMMAND);
 
         return UrlFactory.parameterizeUrl(baseUrl, parameters);
     }
