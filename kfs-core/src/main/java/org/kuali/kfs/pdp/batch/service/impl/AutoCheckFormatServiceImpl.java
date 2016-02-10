7a4ee5998478bc6d4608b4e32aacac50d86f1344
warheadwl <wliang@uci.edu>
2016-02-10 11:36:18 -0800

    KFSCD-51 convert tabs to spaces

--- kfs-core/src/main/java/org/kuali/kfs/pdp/batch/service/impl/AutoCheckFormatServiceImpl.java
+++ kfs-core/src/main/java/org/kuali/kfs/pdp/batch/service/impl/AutoCheckFormatServiceImpl.java
@@ -20,75 +20,75 @@ import org.kuali.rice.location.api.campus.Campus;
 import org.kuali.rice.location.api.campus.CampusService;
 
 public class AutoCheckFormatServiceImpl implements AutoCheckFormatService {
-	private static org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(AutoCheckFormatServiceImpl.class);
-	
-	private FormatService formatService;
-	private DateTimeService dateTimeService;
-	private CampusService campusService;
-	
-	/* (non-Javadoc)
-	 * @see edu.uci.kfs.pdp.batch.service.AutoCheckFormatService#processChecks()
-	 */
-	@Override
-	public boolean processChecks() {
+    private static org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(AutoCheckFormatServiceImpl.class);
+    
+    private FormatService formatService;
+    private DateTimeService dateTimeService;
+    private CampusService campusService;
+    
+    /* (non-Javadoc)
+     * @see edu.uci.kfs.pdp.batch.service.AutoCheckFormatService#processChecks()
+     */
+    @Override
+    public boolean processChecks() {
 
-		List<Campus> findAllCampuses = getCampusService().findAllCampuses();
-		
-		// Go through all the campuses and process the checks ready for format
-		for(Campus campus : findAllCampuses){
-			String campusCode = campus.getCode();
-			
-			// Create FormatSelection object for current campus
-	        FormatSelection formatSelection = getFormatService().getDataForFormat(campusCode);
-	        
-	        // Using the formatSelection object, create a
-			AutoCheckFormat autoFormat = createAutoCheckFormat(formatSelection);
-			
-			if(ObjectUtils.isNull(autoFormat))
-				return false;
-	
-			formatChecks(autoFormat);
-		}
-		
-		return true; 
-	}
+        List<Campus> findAllCampuses = getCampusService().findAllCampuses();
+        
+        // Go through all the campuses and process the checks ready for format
+        for(Campus campus : findAllCampuses){
+            String campusCode = campus.getCode();
+            
+            // Create FormatSelection object for current campus
+            FormatSelection formatSelection = getFormatService().getDataForFormat(campusCode);
+            
+            // Using the formatSelection object, create a
+            AutoCheckFormat autoFormat = createAutoCheckFormat(formatSelection);
+            
+            if(ObjectUtils.isNull(autoFormat))
+                return false;
+    
+            formatChecks(autoFormat);
+        }
+        
+        return true; 
+    }
 
 
-	/**
-	 * @param campusCode
-	 * @return
-	 */
-	protected FormatSelection generateFormatSelectionForCampus(String campusCode) {
-		
-		Date formatStartDate = getFormatService().getFormatProcessStartDate(campusCode);
+    /**
+     * @param campusCode
+     * @return
+     */
+    protected FormatSelection generateFormatSelectionForCampus(String campusCode) {
+        
+        Date formatStartDate = getFormatService().getFormatProcessStartDate(campusCode);
 
-		// create new FormatSelection object an set the campus code and the start date
-		FormatSelection formatSelection = new FormatSelection();
-		formatSelection.setCampus(campusCode);
-		formatSelection.setStartDate(formatStartDate);
+        // create new FormatSelection object an set the campus code and the start date
+        FormatSelection formatSelection = new FormatSelection();
+        formatSelection.setCampus(campusCode);
+        formatSelection.setStartDate(formatStartDate);
 
-		// if format process not started yet, populate the other data as well
-		if (formatStartDate == null) {
-		    formatSelection.setCustomerList(getFormatService().getAllCustomerProfiles());
-		    formatSelection.setRangeList(getFormatService().getAllDisbursementNumberRanges());
-		}
-		return formatSelection;
-	}
+        // if format process not started yet, populate the other data as well
+        if (formatStartDate == null) {
+            formatSelection.setCustomerList(getFormatService().getAllCustomerProfiles());
+            formatSelection.setRangeList(getFormatService().getAllDisbursementNumberRanges());
+        }
+        return formatSelection;
+    }
 
-	
-	/**
-	 *  Check format process - Common process for any type of implementation we want to use
-	 * @param autoFormat
-	 * @return
-	 */
-	protected boolean  formatChecks(AutoCheckFormat autoFormat) {
-		
-	    // Mark payments for format, if there are no payments for format then end the job
-		if (!markPaymentsForFormat(autoFormat))
-			return true;
+    
+    /**
+     *  Check format process - Common process for any type of implementation we want to use
+     * @param autoFormat
+     * @return
+     */
+    protected boolean  formatChecks(AutoCheckFormat autoFormat) {
+        
+        // Mark payments for format, if there are no payments for format then end the job
+        if (!markPaymentsForFormat(autoFormat))
+            return true;
 
-		// Format checks
-		KualiInteger processId = autoFormat.getFormatProcessSummary().getProcessId();
+        // Format checks
+        KualiInteger processId = autoFormat.getFormatProcessSummary().getProcessId();
 
         try {
             getFormatService().performFormat(processId.intValue());
@@ -96,45 +96,45 @@ public class AutoCheckFormatServiceImpl implements AutoCheckFormatService {
             return true;
         }
         catch (FormatException e) {
-        	LOG.error("ERROR AutoCheckFormatStep: " + e.getMessage(), e);
-        	return false;
+            LOG.error("ERROR AutoCheckFormatStep: " + e.getMessage(), e);
+            return false;
         }
-	}
+    }
 
-	/**
-	 * @param autoFormat
-	 * @return
-	 */
-	protected boolean markPaymentsForFormat(AutoCheckFormat autoFormat) {
-		try {
-			Date paymentDate = getDateTimeService().convertToSqlDate(autoFormat.getPaymentDate());
-			FormatProcessSummary formatProcessSummary = getFormatService().startFormatProcess(GlobalVariables.getUserSession().getPerson(), autoFormat.getCampus(), autoFormat.getCustomers(), paymentDate, autoFormat.getPaymentTypes());
-			
-			if (formatProcessSummary.getProcessSummaryList().size() == 0) {
-				LOG.error("Warning AutoCheckFormatStep: There are no payments that match your selection for format process.(Campus Code="+autoFormat.getCampus()+")");
-				return false;
-			}
-			
-			autoFormat.setFormatProcessSummary(formatProcessSummary);
-			return true;
-			
-		} catch (Exception e) {
-			LOG.error("ERROR AutoCheckFormatStep: " + e.getMessage(), e);
-			return false;
-		}
-	}
+    /**
+     * @param autoFormat
+     * @return
+     */
+    protected boolean markPaymentsForFormat(AutoCheckFormat autoFormat) {
+        try {
+            Date paymentDate = getDateTimeService().convertToSqlDate(autoFormat.getPaymentDate());
+            FormatProcessSummary formatProcessSummary = getFormatService().startFormatProcess(GlobalVariables.getUserSession().getPerson(), autoFormat.getCampus(), autoFormat.getCustomers(), paymentDate, autoFormat.getPaymentTypes());
+            
+            if (formatProcessSummary.getProcessSummaryList().size() == 0) {
+                LOG.error("Warning AutoCheckFormatStep: There are no payments that match your selection for format process.(Campus Code="+autoFormat.getCampus()+")");
+                return false;
+            }
+            
+            autoFormat.setFormatProcessSummary(formatProcessSummary);
+            return true;
+            
+        } catch (Exception e) {
+            LOG.error("ERROR AutoCheckFormatStep: " + e.getMessage(), e);
+            return false;
+        }
+    }
 
-	/**
-	 * Creates AutoCheckFormat object that is 
-	 * @param formatSelection
-	 * @return AutoCheckFormat
-	 */
-	@SuppressWarnings("unchecked")
-	protected AutoCheckFormat createAutoCheckFormat(FormatSelection formatSelection) {
+    /**
+     * Creates AutoCheckFormat object that is 
+     * @param formatSelection
+     * @return AutoCheckFormat
+     */
+    @SuppressWarnings("unchecked")
+    protected AutoCheckFormat createAutoCheckFormat(FormatSelection formatSelection) {
         AutoCheckFormat autoFormat = new AutoCheckFormat();
         
         if (ObjectUtils.isNotNull(formatSelection.getStartDate())) {
-        	LOG.error("ERROR AutoCheckFormatStep: The format process is already running. It began at: " + getDateTimeService().toDateTimeString(formatSelection.getStartDate()));
+            LOG.error("ERROR AutoCheckFormatStep: The format process is already running. It began at: " + getDateTimeService().toDateTimeString(formatSelection.getStartDate()));
             return null;
         }
         
@@ -146,20 +146,20 @@ public class AutoCheckFormatServiceImpl implements AutoCheckFormatService {
         List<CustomerProfile> customers = generateListOfCustomerProfilesReadyForFormat(formatSelection);
         autoFormat.setCustomers(customers);
     
-		return autoFormat;
-	}
+        return autoFormat;
+    }
 
 
-	/**
-	 * Return a list of CustomerProfile that are eligible for format
-	 * @param formatSelection
-	 * @return List<CustomerProfile>
-	 */
-	protected List<CustomerProfile> generateListOfCustomerProfilesReadyForFormat(FormatSelection formatSelection) {
-		List<CustomerProfile> customers = formatSelection.getCustomerList();
+    /**
+     * Return a list of CustomerProfile that are eligible for format
+     * @param formatSelection
+     * @return List<CustomerProfile>
+     */
+    protected List<CustomerProfile> generateListOfCustomerProfilesReadyForFormat(FormatSelection formatSelection) {
+        List<CustomerProfile> customers = formatSelection.getCustomerList();
 
-		if(ObjectUtils.isNull(customers)) return new ArrayList<CustomerProfile>();
-		
+        if(ObjectUtils.isNull(customers)) return new ArrayList<CustomerProfile>();
+        
         for (CustomerProfile element : customers) {
 
             if (formatSelection.getCampus().equals(element.getDefaultPhysicalCampusProcessingCode())) {
@@ -169,52 +169,52 @@ public class AutoCheckFormatServiceImpl implements AutoCheckFormatService {
                 element.setSelectedForFormat(Boolean.FALSE);
             }
         }
-		return customers;
-	}
+        return customers;
+    }
 
 
-	/**
-	 * @return the formatService
-	 */
-	public FormatService getFormatService() {
-		return formatService;
-	}
+    /**
+     * @return the formatService
+     */
+    public FormatService getFormatService() {
+        return formatService;
+    }
 
-	/**
-	 * @param formatService the formatService to set
-	 */
-	public void setFormatService(FormatService formatService) {
-		this.formatService = formatService;
-	}
+    /**
+     * @param formatService the formatService to set
+     */
+    public void setFormatService(FormatService formatService) {
+        this.formatService = formatService;
+    }
 
-	/**
-	 * @return the dateTimeService
-	 */
-	public DateTimeService getDateTimeService() {
-		return dateTimeService;
-	}
+    /**
+     * @return the dateTimeService
+     */
+    public DateTimeService getDateTimeService() {
+        return dateTimeService;
+    }
 
-	/**
-	 * @param dateTimeService the dateTimeService to set
-	 */
-	public void setDateTimeService(DateTimeService dateTimeService) {
-		this.dateTimeService = dateTimeService;
-	}
-
-
-	/**
-	 * @return the campusService
-	 */
-	public CampusService getCampusService() {
-		return campusService;
-	}
+    /**
+     * @param dateTimeService the dateTimeService to set
+     */
+    public void setDateTimeService(DateTimeService dateTimeService) {
+        this.dateTimeService = dateTimeService;
+    }
 
 
-	/**
-	 * @param campusService the campusService to set
-	 */
-	public void setCampusService(CampusService campusService) {
-		this.campusService = campusService;
-	}
+    /**
+     * @return the campusService
+     */
+    public CampusService getCampusService() {
+        return campusService;
+    }
+
+
+    /**
+     * @param campusService the campusService to set
+     */
+    public void setCampusService(CampusService campusService) {
+        this.campusService = campusService;
+    }
 
 }
