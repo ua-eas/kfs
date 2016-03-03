2f32b7ffc0f6738632219acf1cf05f2e92000c1a
desizombi3 <bhargav.pate@uci.edu>
2016-03-03 11:27:17 -0800

    added a new junit test class

---
+++ kfs-core/src/test/java/org/kuali/kfs/pdp/service/impl/FormatServiceImplTest.java
@@ -0,0 +1,14 @@
+package org.kuali.kfs.pdp.service.impl;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FormatServiceImplTest {
+
+	@Test
+	public void testAddSummaryToCustomerProfileMap() {
+		fail("Not yet implemented");
+	}
+
+}
