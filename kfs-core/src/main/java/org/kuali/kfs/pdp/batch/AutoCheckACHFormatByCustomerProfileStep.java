9467374974595921d26a49adc4ffc02dc4088f23
desizombi3 <bhargav.pate@uci.edu>
2016-03-07 14:39:04 -0800

    get cutomer profile ID from parameter

--- kfs-core/src/main/java/org/kuali/kfs/pdp/batch/AutoCheckACHFormatByCustomerProfileStep.java
+++ kfs-core/src/main/java/org/kuali/kfs/pdp/batch/AutoCheckACHFormatByCustomerProfileStep.java
@@ -5,31 +5,47 @@ import java.util.Date;
 import org.kuali.kfs.coreservice.framework.parameter.ParameterService;
 import org.kuali.kfs.pdp.PdpConstants;
 import org.kuali.kfs.pdp.batch.service.AutoCheckFormatService;
+import org.kuali.kfs.sys.KFSConstants;
 import org.kuali.kfs.sys.batch.AbstractStep;
-import org.kuali.kfs.sys.service.impl.KfsParameterConstants;
 
 public class AutoCheckACHFormatByCustomerProfileStep extends AbstractStep {
 
 	private AutoCheckFormatService autoCheckFormatService;
 	private ParameterService parameterService;
-	
-	/* (non-Javadoc)
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.kuali.kfs.kns.bo.Step#execute(java.lang.String, java.util.Date)
 	 */
 	@Override
-	public boolean execute(String jobName, Date jobRunDate) throws InterruptedException {
-		
-		//checks selected will be based on customer profile set in parameter. If value is null, then ALL customer profiles will be selected.
-		String customerProfileId = getParameterService().getParameterValueAsString(KfsParameterConstants.PRE_DISBURSEMENT_BATCH.class, PdpConstants.FORMAT_CUSTOMER_PROFILE_ID);
-		
-		return autoCheckFormatService.processChecksByCustomerProfile(customerProfileId);
+	public boolean execute(String jobName, Date jobRunDate)
+			throws InterruptedException {
+
+		// checks selected will be based on customer profile set in parameter.
+		// If value is null, then ALL customer profiles will be selected.
+		String customerProfileId = getParameterService()
+				.getParameterValueAsString(
+						KFSConstants.CoreModuleNamespaces.PDP,
+						PdpConstants.FormatCheckACHParameters.PDP_FORMAT_CHECK_ACH_BY_CUST_PROF_STEP,
+						PdpConstants.FORMAT_CUSTOMER_PROFILE_ID);
+
+		return autoCheckFormatService
+				.processChecksByCustomerProfile(customerProfileId);
 	}
 
+	/**
+	 * @return
+	 */
 	public AutoCheckFormatService getAutoCheckFormatService() {
 		return autoCheckFormatService;
 	}
 
-	public void setAutoCheckFormatService(AutoCheckFormatService autoCheckFormatService) {
+	/**
+	 * @param autoCheckFormatService
+	 */
+	public void setAutoCheckFormatService(
+			AutoCheckFormatService autoCheckFormatService) {
 		this.autoCheckFormatService = autoCheckFormatService;
 	}
 
@@ -41,7 +57,8 @@ public class AutoCheckACHFormatByCustomerProfileStep extends AbstractStep {
 	}
 
 	/**
-	 * @param parameterService the parameterService to set
+	 * @param parameterService
+	 *            the parameterService to set
 	 */
 	public void setParameterService(ParameterService parameterService) {
 		this.parameterService = parameterService;
