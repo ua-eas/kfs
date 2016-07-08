d05f95e675991085025d1d51cbf0ff2e64cee834
Jay Sissom <jsissom@jsissom-mbp.attlocal.net>
2016-07-08 08:04:41 -0400

    FINI-2083 Display proper error message to end user

---
+++ kfs-core/src/main/java/org/kuali/kfs/sys/exception/DisplayMessageException.java
@@ -0,0 +1,8 @@
+package org.kuali.kfs.sys.exception;
+
+public interface DisplayMessageException {
+    /**
+     * This is a tag interface to denote an exception that, when passed up to Struts, should always display the message
+     * to the end user.  It is used in ErrorHandlerAction.
+     */
+}
--- kfs-kns/src/main/java/org/kuali/kfs/kns/util/IncidentReportUtils.java
+++ kfs-kns/src/main/java/org/kuali/kfs/kns/util/IncidentReportUtils.java
@@ -1,68 +1,56 @@
 /*
  * The Kuali Financial System, a comprehensive financial management system for higher education.
- * 
+ *
  * Copyright 2005-2015 The Kuali Foundation
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.kuali.kfs.kns.util;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-
-import org.apache.struts.Globals;
-import org.kuali.rice.kim.api.identity.Person;
 import org.kuali.kfs.krad.UserSession;
 import org.kuali.kfs.krad.exception.KualiExceptionIncident;
 import org.kuali.kfs.krad.util.KRADConstants;
+import org.kuali.rice.kim.api.identity.Person;
 
-/**
- * This is a description of what this class does - ewestfal don't forget to fill
- * this in.
- * 
- * @author Kuali Rice Team (rice.collab@kuali.org)
- * 
- */
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
+
 public final class IncidentReportUtils {
-
 	/**
      * Key to define the attribute stores exception properties such as
      * user email, user name, component name, etc.
      * <p>Value is exceptionProperties
      */
-    public static final String EXCEPTION_PROPERTIES="exceptionProperties";
-    
+    public static final String EXCEPTION_PROPERTIES = "exceptionProperties";
+
 	private IncidentReportUtils() {
 		throw new UnsupportedOperationException("do not call");
 	}
 
-	public static Map<String, String> populateRequestForIncidentReport(Exception exception,
-			String documentId, String componentName, HttpServletRequest request) {
-
+	public static Map<String, String> populateRequestForIncidentReport(String documentId, String componentName, HttpServletRequest request) {
 		// Create properties of form and user for additional information
 		// to be displayed or passing through JSP
-		Map<String, String> properties = new HashMap<String, String>();
+		Map<String, String> properties = new HashMap<>();
 		properties.put(KualiExceptionIncident.DOCUMENT_ID, documentId);
 		String userEmail = "";
 		String userName = "";
 		String uuid = "";
+
 		// No specific forward for the caught exception, use default logic
 		// Get user information
-		UserSession userSession = (UserSession) request.getSession()
-				.getAttribute(KRADConstants.USER_SESSION_KEY);
+		UserSession userSession = (UserSession) request.getSession().getAttribute(KRADConstants.USER_SESSION_KEY);
 		Person sessionUser = null;
 		if (userSession != null) {
 			sessionUser = userSession.getPerson();
@@ -77,12 +65,6 @@ public final class IncidentReportUtils {
 		properties.put(KualiExceptionIncident.UUID, uuid);
 		properties.put(KualiExceptionIncident.COMPONENT_NAME, componentName);
 
-		// Reset the exception so the forward action can read it
-		request.setAttribute(Globals.EXCEPTION_KEY, exception);
-		// Set exception current information
-		request.setAttribute(EXCEPTION_PROPERTIES, properties);
-
 		return properties;
-
 	}
 }
