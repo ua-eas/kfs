d98c24ebf3adb385b3fbd8f8982f3e41c8c66aff
Travis Foxley <travisfoxley@gmail.com>
2016-10-24 13:14:26 -0600

    FINP-784 re-enable search api tests (#902)
    * FINP-784 re-enable search api tests
    
    * FINP-784 fix demo.properties

--- kfs-api-test/src/test/java/org/kuali/kfs/apitest/test/DataValidationTest.java
+++ kfs-api-test/src/test/java/org/kuali/kfs/apitest/test/DataValidationTest.java
@@ -26,11 +26,15 @@ import org.kuali.kfs.apitest.Constants;
 import org.kuali.kfs.apitest.utils.RestUtilities;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class DataValidationTest {
     private static String API = "/api/v1/business-object/ar/system-informations/5A691AF93B5A424DE0404F8189D80D73";
-//    private static String SEARCH_API = "/api/v1/business-object/ar/system-informations";
+    private static String SEARCH_API = "/api/v1/business-object/ar/system-informations";
 
     @Test
     public void validModuleName() throws IOException {
@@ -55,133 +59,133 @@ public class DataValidationTest {
         Assert.assertEquals(1,versionNumber);
     }
 
-//    @Test
-//    public void validSearchModuleName() throws IOException {
-//        HttpResponse response = RestUtilities.makeRequest(SEARCH_API + "?objectId=5A691AF93B5A424DE0404F8189D80D73", Constants.KHUNTLEY_TOKEN);
-//
-//        Assert.assertEquals(HttpStatus.SC_OK,response.getStatusLine().getStatusCode());
-//
-//        Map<String,Object> searchResults = RestUtilities.parse(RestUtilities.inputStreamToString(response.getEntity().getContent()));
-//        List<Map<String, Object>> results = (List<Map<String, Object>>)searchResults.get(Constants.Search.RESULTS);
-//        Map<String, Object> bo = results.get(0);
-//
-//        // Validate that the returned object is correct
-//        Assert.assertEquals(new ArrayList(Arrays.asList("objectId")), searchResults.get(Constants.Search.SORT));
-//        Assert.assertEquals(200, searchResults.get(Constants.Search.LIMIT));
-//        Assert.assertEquals(0, searchResults.get(Constants.Search.SKIP));
-//        Assert.assertEquals(1, searchResults.get(Constants.Search.TOTAL_COUNT));
-//        Map<String, String> query = new HashMap<String, String>();
-//        query.put("objectId", "5A691AF93B5A424DE0404F8189D80D73");
-//        Assert.assertEquals(query, searchResults.get(Constants.Search.QUERY));
-//
-//        // Check a sample of fields returned
-//        Assert.assertEquals(true,(Boolean)bo.get("active"));
-//        Assert.assertEquals("9460",(String)bo.get("creditCardObjectCode"));
-//        Assert.assertEquals("66255",(String)bo.get("lockboxNumber"));
-//        Assert.assertEquals(false,(Boolean)bo.get("newCollectionRecord"));
-//        Assert.assertEquals("5A691AF93B5A424DE0404F8189D80D73",(String)bo.get("objectId"));
-//
-//        Map<String,Object> processingChartOfAccount = (Map<String,Object>)bo.get("processingChartOfAccount");
-//        String link = (String)processingChartOfAccount.get("link");
-//        Assert.assertTrue(link.endsWith("/api/v1/business-object/coa/charts/014F3DAF748BA448E043814FD28EA448"));
-//
-//        int versionNumber = (Integer)bo.get("versionNumber");
-//        Assert.assertEquals(1,versionNumber);
-//    }
+    @Test
+    public void validSearchModuleName() throws IOException {
+        HttpResponse response = RestUtilities.makeRequest(SEARCH_API + "?objectId=5A691AF93B5A424DE0404F8189D80D73", Constants.KHUNTLEY_TOKEN);
 
-//    @Test
-//    public void validSearchWithLimitAndSkip() throws IOException {
-//        HttpResponse response = RestUtilities.makeRequest(SEARCH_API + "?limit=2&skip=1", Constants.KHUNTLEY_TOKEN);
-//
-//        Assert.assertEquals(HttpStatus.SC_OK,response.getStatusLine().getStatusCode());
-//
-//        Map<String,Object> searchResults = RestUtilities.parse(RestUtilities.inputStreamToString(response.getEntity().getContent()));
-//        List<Map<String, Object>> results = (List<Map<String, Object>>)searchResults.get(Constants.Search.RESULTS);
-//        Map<String, Object> bo = results.get(0);
-//
-//        // Validate that the returned object is correct
-//        Assert.assertEquals(new ArrayList(Arrays.asList("objectId")), searchResults.get(Constants.Search.SORT));
-//        Assert.assertEquals(2, searchResults.get(Constants.Search.LIMIT));
-//        Assert.assertEquals(1, searchResults.get(Constants.Search.SKIP));
-//        Assert.assertEquals(60, searchResults.get(Constants.Search.TOTAL_COUNT));
-//        Assert.assertEquals(new HashMap(), searchResults.get(Constants.Search.QUERY));
-//
-//        // Check a sample of fields returned
-//        Assert.assertEquals(true,(Boolean)bo.get("active"));
-//        Assert.assertEquals("9460",(String)bo.get("creditCardObjectCode"));
-//        Assert.assertEquals("66255",(String)bo.get("lockboxNumber"));
-//        Assert.assertEquals(false,(Boolean)bo.get("newCollectionRecord"));
-//        Assert.assertEquals("03550bdf-2b67-48a3-bafe-dd431d017059",(String)bo.get("objectId"));
-//
-//        Map<String,Object> processingChartOfAccount = (Map<String,Object>)bo.get("processingChartOfAccount");
-//        String link = (String)processingChartOfAccount.get("link");
-//        Assert.assertTrue(link.endsWith("/api/v1/business-object/coa/charts/014F3DAF748BA448E043814FD28EA448"));
-//
-//        int versionNumber = (Integer)bo.get("versionNumber");
-//        Assert.assertEquals(2,versionNumber);
-//    }
+        Assert.assertEquals(HttpStatus.SC_OK,response.getStatusLine().getStatusCode());
 
-//    @Test
-//    public void validSearchWithSort() throws IOException {
-//        HttpResponse response = RestUtilities.makeRequest(SEARCH_API + "?limit=1&sort=-objectId", Constants.KHUNTLEY_TOKEN);
-//
-//        Assert.assertEquals(HttpStatus.SC_OK,response.getStatusLine().getStatusCode());
-//
-//        Map<String,Object> searchResults = RestUtilities.parse(RestUtilities.inputStreamToString(response.getEntity().getContent()));
-//        List<Map<String, Object>> results = (List<Map<String, Object>>)searchResults.get(Constants.Search.RESULTS);
-//        Map<String, Object> bo = results.get(0);
-//
-//        // Validate that the returned object is correct
-//        Assert.assertEquals(new ArrayList(Arrays.asList("-objectId")), searchResults.get(Constants.Search.SORT));
-//        Assert.assertEquals(1, searchResults.get(Constants.Search.LIMIT));
-//        Assert.assertEquals(0, searchResults.get(Constants.Search.SKIP));
-//        Assert.assertEquals(60, searchResults.get(Constants.Search.TOTAL_COUNT));
-//        Assert.assertEquals(new HashMap(), searchResults.get(Constants.Search.QUERY));
-//
-//        // Check a sample of fields returned
-//        Assert.assertEquals(true,(Boolean)bo.get("active"));
-//        Assert.assertEquals("1599",(String)bo.get("creditCardObjectCode"));
-//        Assert.assertEquals("66248",(String)bo.get("lockboxNumber"));
-//        Assert.assertEquals(false,(Boolean)bo.get("newCollectionRecord"));
-//        Assert.assertEquals("fde14bd6-73b1-48c3-8939-20909f0f3459",(String)bo.get("objectId"));
-//
-//        Map<String,Object> processingChartOfAccount = (Map<String,Object>)bo.get("processingChartOfAccount");
-//        String link = (String)processingChartOfAccount.get("link");
-//        Assert.assertTrue(link.endsWith("/api/v1/business-object/coa/charts/014F3DAF7495A448E043814FD28EA448"));
-//
-//        int versionNumber = (Integer)bo.get("versionNumber");
-//        Assert.assertEquals(2,versionNumber);
-//    }
+        Map<String,Object> searchResults = RestUtilities.parse(RestUtilities.inputStreamToString(response.getEntity().getContent()));
+        List<Map<String, Object>> results = (List<Map<String, Object>>)searchResults.get(Constants.Search.RESULTS);
+        Map<String, Object> bo = results.get(0);
 
-//    @Test
-//    public void validSearchWithoutLimit() throws IOException {
-//        HttpResponse response = RestUtilities.makeRequest(SEARCH_API, Constants.KHUNTLEY_TOKEN);
-//
-//        Assert.assertEquals(HttpStatus.SC_OK,response.getStatusLine().getStatusCode());
-//
-//        Map<String,Object> searchResults = RestUtilities.parse(RestUtilities.inputStreamToString(response.getEntity().getContent()));
-//        List<Map<String, Object>> results = (List<Map<String, Object>>)searchResults.get(Constants.Search.RESULTS);
-//        Map<String, Object> bo = results.get(0);
-//
-//        // Validate that the returned object is correct
-//        Assert.assertEquals(new ArrayList(Arrays.asList("objectId")), searchResults.get(Constants.Search.SORT));
-//        Assert.assertEquals(200, searchResults.get(Constants.Search.LIMIT));
-//        Assert.assertEquals(0, searchResults.get(Constants.Search.SKIP));
-//        Assert.assertEquals(60, searchResults.get(Constants.Search.TOTAL_COUNT));
-//        Assert.assertEquals(new HashMap(), searchResults.get(Constants.Search.QUERY));
-//
-//        // Check a sample of fields returned
-//        Assert.assertEquals(true,(Boolean)bo.get("active"));
-//        Assert.assertEquals("9460",(String)bo.get("creditCardObjectCode"));
-//        Assert.assertEquals("66255",(String)bo.get("lockboxNumber"));
-//        Assert.assertEquals(false,(Boolean)bo.get("newCollectionRecord"));
-//        Assert.assertEquals("026b0c29-04a8-4b4e-a6b0-c7dc8a641a44",(String)bo.get("objectId"));
-//
-//        Map<String,Object> processingChartOfAccount = (Map<String,Object>)bo.get("processingChartOfAccount");
-//        String link = (String)processingChartOfAccount.get("link");
-//        Assert.assertTrue(link.endsWith("/api/v1/business-object/coa/charts/014F3DAF748BA448E043814FD28EA448"));
-//
-//        int versionNumber = (Integer)bo.get("versionNumber");
-//        Assert.assertEquals(2,versionNumber);
-//    }
+        // Validate that the returned object is correct
+        Assert.assertEquals(new ArrayList(Arrays.asList("objectId")), searchResults.get(Constants.Search.SORT));
+        Assert.assertEquals(200, searchResults.get(Constants.Search.LIMIT));
+        Assert.assertEquals(0, searchResults.get(Constants.Search.SKIP));
+        Assert.assertEquals(1, searchResults.get(Constants.Search.TOTAL_COUNT));
+        Map<String, String> query = new HashMap<String, String>();
+        query.put("objectId", "5A691AF93B5A424DE0404F8189D80D73");
+        Assert.assertEquals(query, searchResults.get(Constants.Search.QUERY));
+
+        // Check a sample of fields returned
+        Assert.assertEquals(true,(Boolean)bo.get("active"));
+        Assert.assertEquals("9460",(String)bo.get("creditCardObjectCode"));
+        Assert.assertEquals("66255",(String)bo.get("lockboxNumber"));
+        Assert.assertEquals(false,(Boolean)bo.get("newCollectionRecord"));
+        Assert.assertEquals("5A691AF93B5A424DE0404F8189D80D73",(String)bo.get("objectId"));
+
+        Map<String,Object> processingChartOfAccount = (Map<String,Object>)bo.get("processingChartOfAccount");
+        String link = (String)processingChartOfAccount.get("link");
+        Assert.assertTrue(link.endsWith("/api/v1/business-object/coa/charts/014F3DAF748BA448E043814FD28EA448"));
+
+        int versionNumber = (Integer)bo.get("versionNumber");
+        Assert.assertEquals(1,versionNumber);
+    }
+
+    @Test
+    public void validSearchWithLimitAndSkip() throws IOException {
+        HttpResponse response = RestUtilities.makeRequest(SEARCH_API + "?limit=2&skip=1", Constants.KHUNTLEY_TOKEN);
+
+        Assert.assertEquals(HttpStatus.SC_OK,response.getStatusLine().getStatusCode());
+
+        Map<String,Object> searchResults = RestUtilities.parse(RestUtilities.inputStreamToString(response.getEntity().getContent()));
+        List<Map<String, Object>> results = (List<Map<String, Object>>)searchResults.get(Constants.Search.RESULTS);
+        Map<String, Object> bo = results.get(0);
+
+        // Validate that the returned object is correct
+        Assert.assertEquals(new ArrayList(Arrays.asList("objectId")), searchResults.get(Constants.Search.SORT));
+        Assert.assertEquals(2, searchResults.get(Constants.Search.LIMIT));
+        Assert.assertEquals(1, searchResults.get(Constants.Search.SKIP));
+        Assert.assertEquals(60, searchResults.get(Constants.Search.TOTAL_COUNT));
+        Assert.assertEquals(new HashMap(), searchResults.get(Constants.Search.QUERY));
+
+        // Check a sample of fields returned
+        Assert.assertEquals(true,(Boolean)bo.get("active"));
+        Assert.assertEquals("9460",(String)bo.get("creditCardObjectCode"));
+        Assert.assertEquals("66255",(String)bo.get("lockboxNumber"));
+        Assert.assertEquals(false,(Boolean)bo.get("newCollectionRecord"));
+        Assert.assertEquals("03550bdf-2b67-48a3-bafe-dd431d017059",(String)bo.get("objectId"));
+
+        Map<String,Object> processingChartOfAccount = (Map<String,Object>)bo.get("processingChartOfAccount");
+        String link = (String)processingChartOfAccount.get("link");
+        Assert.assertTrue(link.endsWith("/api/v1/business-object/coa/charts/014F3DAF748BA448E043814FD28EA448"));
+
+        int versionNumber = (Integer)bo.get("versionNumber");
+        Assert.assertEquals(2,versionNumber);
+    }
+
+    @Test
+    public void validSearchWithSort() throws IOException {
+        HttpResponse response = RestUtilities.makeRequest(SEARCH_API + "?limit=1&sort=-objectId", Constants.KHUNTLEY_TOKEN);
+
+        Assert.assertEquals(HttpStatus.SC_OK,response.getStatusLine().getStatusCode());
+
+        Map<String,Object> searchResults = RestUtilities.parse(RestUtilities.inputStreamToString(response.getEntity().getContent()));
+        List<Map<String, Object>> results = (List<Map<String, Object>>)searchResults.get(Constants.Search.RESULTS);
+        Map<String, Object> bo = results.get(0);
+
+        // Validate that the returned object is correct
+        Assert.assertEquals(new ArrayList(Arrays.asList("-objectId")), searchResults.get(Constants.Search.SORT));
+        Assert.assertEquals(1, searchResults.get(Constants.Search.LIMIT));
+        Assert.assertEquals(0, searchResults.get(Constants.Search.SKIP));
+        Assert.assertEquals(60, searchResults.get(Constants.Search.TOTAL_COUNT));
+        Assert.assertEquals(new HashMap(), searchResults.get(Constants.Search.QUERY));
+
+        // Check a sample of fields returned
+        Assert.assertEquals(true,(Boolean)bo.get("active"));
+        Assert.assertEquals("1599",(String)bo.get("creditCardObjectCode"));
+        Assert.assertEquals("66248",(String)bo.get("lockboxNumber"));
+        Assert.assertEquals(false,(Boolean)bo.get("newCollectionRecord"));
+        Assert.assertEquals("fde14bd6-73b1-48c3-8939-20909f0f3459",(String)bo.get("objectId"));
+
+        Map<String,Object> processingChartOfAccount = (Map<String,Object>)bo.get("processingChartOfAccount");
+        String link = (String)processingChartOfAccount.get("link");
+        Assert.assertTrue(link.endsWith("/api/v1/business-object/coa/charts/014F3DAF7495A448E043814FD28EA448"));
+
+        int versionNumber = (Integer)bo.get("versionNumber");
+        Assert.assertEquals(2,versionNumber);
+    }
+
+    @Test
+    public void validSearchWithoutLimit() throws IOException {
+        HttpResponse response = RestUtilities.makeRequest(SEARCH_API, Constants.KHUNTLEY_TOKEN);
+
+        Assert.assertEquals(HttpStatus.SC_OK,response.getStatusLine().getStatusCode());
+
+        Map<String,Object> searchResults = RestUtilities.parse(RestUtilities.inputStreamToString(response.getEntity().getContent()));
+        List<Map<String, Object>> results = (List<Map<String, Object>>)searchResults.get(Constants.Search.RESULTS);
+        Map<String, Object> bo = results.get(0);
+
+        // Validate that the returned object is correct
+        Assert.assertEquals(new ArrayList(Arrays.asList("objectId")), searchResults.get(Constants.Search.SORT));
+        Assert.assertEquals(200, searchResults.get(Constants.Search.LIMIT));
+        Assert.assertEquals(0, searchResults.get(Constants.Search.SKIP));
+        Assert.assertEquals(60, searchResults.get(Constants.Search.TOTAL_COUNT));
+        Assert.assertEquals(new HashMap(), searchResults.get(Constants.Search.QUERY));
+
+        // Check a sample of fields returned
+        Assert.assertEquals(true,(Boolean)bo.get("active"));
+        Assert.assertEquals("9460",(String)bo.get("creditCardObjectCode"));
+        Assert.assertEquals("66255",(String)bo.get("lockboxNumber"));
+        Assert.assertEquals(false,(Boolean)bo.get("newCollectionRecord"));
+        Assert.assertEquals("026b0c29-04a8-4b4e-a6b0-c7dc8a641a44",(String)bo.get("objectId"));
+
+        Map<String,Object> processingChartOfAccount = (Map<String,Object>)bo.get("processingChartOfAccount");
+        String link = (String)processingChartOfAccount.get("link");
+        Assert.assertTrue(link.endsWith("/api/v1/business-object/coa/charts/014F3DAF748BA448E043814FD28EA448"));
+
+        int versionNumber = (Integer)bo.get("versionNumber");
+        Assert.assertEquals(2,versionNumber);
+    }
 }
