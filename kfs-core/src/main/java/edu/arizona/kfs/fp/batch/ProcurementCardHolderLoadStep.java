b537c5f5450662cbdc1021a9f44909c30b584d76
Mark Moen <mmoen@eaglecrk.com>
2017-01-06 14:55:13 -0600

    UAF-2812 added some missing code from kfs 3 on ProcurementCardHolderLoadStep. Moved ProcurementCardHolderUpdateServiceImpl to the correct folder. Minor improvements

--- kfs-core/src/main/java/edu/arizona/kfs/fp/batch/ProcurementCardHolderLoadStep.java
+++ kfs-core/src/main/java/edu/arizona/kfs/fp/batch/ProcurementCardHolderLoadStep.java
@@ -1,15 +1,16 @@
 package edu.arizona.kfs.fp.batch;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.Date;
+import java.sql.Date;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.lang.StringUtils;
 import org.kuali.kfs.sys.batch.AbstractStep;
 import org.kuali.kfs.sys.batch.BatchInputFileType;
 import org.kuali.kfs.sys.batch.service.BatchInputFileService;
-import org.kuali.kfs.sys.context.SpringContext;
 
 import edu.arizona.kfs.fp.batch.service.ProcurementCardHolderLoadService;
 
@@ -25,40 +26,60 @@ import edu.arizona.kfs.fp.batch.service.ProcurementCardHolderLoadService;
  * transaction. Step can be restarted as needed.
  */
 public class ProcurementCardHolderLoadStep extends AbstractStep {
+    private static org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(ProcurementCardHolderLoadStep.class);
 
     private ProcurementCardHolderLoadService procurementCardHolderLoadService;
     private BatchInputFileService batchInputFileService;
     private BatchInputFileType procurementCardHolderInputFileType;
-
-    public BatchInputFileService getBatchInputFileService() {
-        if (batchInputFileService == null) {
-            batchInputFileService = SpringContext.getBean(BatchInputFileService.class);
-        }
-        return batchInputFileService;
-    }
-
+    
     /**
      * Controls the procurement cardholder load process.
      */
-    public boolean execute(String jobName, Date jobRunDate) throws InterruptedException {
+    public boolean execute(String jobName, java.util.Date jobRunDate) throws InterruptedException {
         procurementCardHolderLoadService.cleanTransactionsTable();
 
-        List<String> fileNamesToLoad = getBatchInputFileService().listInputFileNamesWithDoneFile(procurementCardHolderInputFileType);
+        List<String> fileNamesToLoad = batchInputFileService.listInputFileNamesWithDoneFile(procurementCardHolderInputFileType);
 
-        boolean processSuccess = true;
-        List<String> processedFiles = new ArrayList<String>();
-        for (String inputFileName : fileNamesToLoad) {
-            processSuccess = procurementCardHolderLoadService.loadProcurementCardHolderFile(inputFileName);
-            if (processSuccess) {
-                processedFiles.add(inputFileName);
+        //process only most recent file set to prevent duplicates
+        Map<Date, String> fileNameMap = buildMapForFileNameList(fileNamesToLoad);
+        Date holdDate = null;
+        for (Iterator<Date> iter2 = fileNameMap.keySet().iterator(); iter2.hasNext();) {
+            Date fileDate = (Date) iter2.next();
+            if (holdDate == null || holdDate.compareTo(fileDate) < 0) {
+                holdDate = fileDate;
             }
         }
+        String holdDateString = (String) fileNameMap.get(holdDate);
+        
+        boolean processSuccess = true;
 
-        removeDoneFiles(processedFiles);
-
+        for (String inputFileName : fileNamesToLoad) {
+            if (inputFileName.contains(holdDateString)) {
+                // Removing related .done file right away
+                removeDoneFile(inputFileName);
+                LOG.info("Loading Procurement Cardholder file " + new File(inputFileName).getName());
+                processSuccess = procurementCardHolderLoadService.loadProcurementCardHolderFile(inputFileName);
+            }
+        }
+        //remove any leftover done files
+        removeDoneFiles(fileNamesToLoad);
         return processSuccess;
     }
-
+    
+    public Map<Date, String> buildMapForFileNameList(List<String> fileNamesToLoad) {
+        Map<Date, String> fileNameMap = new HashMap<Date, String>();
+        File inputFile = null;
+        String shortFileName = null;
+        
+        for (String inputFileName : fileNamesToLoad) {
+            inputFile = new File(inputFileName);
+            shortFileName = inputFile.getName();
+            // place file date values in map
+            fileNameMap.put(Date.valueOf(shortFileName.substring(10, 20)), shortFileName.substring(10, 20));
+        }
+        return fileNameMap;
+    }
+    
     /**
      * Clears out associated .done files for the processed data files.
      */
@@ -70,17 +91,27 @@ public class ProcurementCardHolderLoadStep extends AbstractStep {
             }
         }
     }
-
+    
     /**
-     * Sets the procurementCardHolderInputFileType attribute value.
+     * Clears out the associated .done file for the data file about to be processed.
+     * 
+     * @param dataFileName the name of date file with done file to remove
      */
+    protected void removeDoneFile(String dataFileName) {
+        File doneFile = new File(StringUtils.substringBeforeLast(dataFileName, ".") + ".done");
+        if (doneFile.exists()) {
+            doneFile.delete();
+        }
+    }
+    
+    public void setBatchInputFileService(BatchInputFileService batchInputFileService) {
+        this.batchInputFileService = batchInputFileService;
+    }
+
     public void setProcurementCardHolderInputFileType(BatchInputFileType procurementCardHolderInputFileType) {
         this.procurementCardHolderInputFileType = procurementCardHolderInputFileType;
     }
 
-    /**
-     * Sets the procurementCardHolderLoadService attribute value.
-     */
     public void setProcurementCardHolderLoadService(ProcurementCardHolderLoadService procurementCardHolderLoadService) {
         this.procurementCardHolderLoadService = procurementCardHolderLoadService;
     }
