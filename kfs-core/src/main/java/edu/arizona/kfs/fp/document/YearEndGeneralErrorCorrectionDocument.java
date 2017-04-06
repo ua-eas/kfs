efb6cc43be38183179fffef30b29773518d5a335
Lin-Long Shyu <lshyu@email.arizona.edu>
2017-04-06 12:35:33 -0400

    UAF-3119 : Fixed YEGE import search and search page not consistent; and YEGE GLPE not showing previous FY

--- kfs-core/src/main/java/edu/arizona/kfs/fp/document/YearEndGeneralErrorCorrectionDocument.java
+++ kfs-core/src/main/java/edu/arizona/kfs/fp/document/YearEndGeneralErrorCorrectionDocument.java
@@ -1,11 +1,25 @@
 package edu.arizona.kfs.fp.document;
 
+import org.kuali.kfs.coa.businessobject.AccountingPeriod;
 import org.kuali.kfs.fp.document.YearEndDocument;
+import org.kuali.kfs.fp.document.service.YearEndPendingEntryService;
+import org.kuali.kfs.sys.context.SpringContext;
 
 public class YearEndGeneralErrorCorrectionDocument extends GeneralErrorCorrectionDocument implements YearEndDocument {
 
 	public YearEndGeneralErrorCorrectionDocument() {
 		super();
 	}
-	
+
+    @Override
+    public void setAccountingPeriod(AccountingPeriod accountingPeriod) {
+        //Need to override this method to set the posting year and
+        //posting period code so that we can have period 13 in the document's
+        //table in the database so that it's consistent with the GL Entry's
+        //posting year and posting period.
+        YearEndPendingEntryService yearEndPendingEntryService = SpringContext.getBean(YearEndPendingEntryService.class);
+        setPostingYear(yearEndPendingEntryService.getPreviousFiscalYear());
+        this.setPostingPeriodCode(yearEndPendingEntryService.getFinalAccountingPeriod());
+    }
+
 }
