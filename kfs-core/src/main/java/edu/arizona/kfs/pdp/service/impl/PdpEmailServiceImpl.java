e4d2b416bf4ec11bdf8a320a4b0b4e1db3941a43
Scott G. Skinner <sskinner@email.arizona.edu>
2017-08-09 17:47:51 -0700

    UAF-5298 - Fixing compile errors in PdpEmailServiceImpl, but had to comment out 4-5 lines.

--- kfs-core/src/main/java/edu/arizona/kfs/pdp/service/impl/PdpEmailServiceImpl.java
+++ kfs-core/src/main/java/edu/arizona/kfs/pdp/service/impl/PdpEmailServiceImpl.java
@@ -1,64 +1,47 @@
 package edu.arizona.kfs.pdp.service.impl;
 
-import java.util.*;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
+import edu.arizona.kfs.pdp.service.PdpEmailService;
 import org.apache.commons.lang.StringUtils;
+import org.kuali.kfs.coreservice.framework.parameter.ParameterService;
+import org.kuali.kfs.krad.util.KRADUtils;
 import org.kuali.kfs.pdp.PdpKeyConstants;
 import org.kuali.kfs.pdp.PdpParameterConstants;
-import org.kuali.kfs.pdp.PdpPropertyConstants;
 import org.kuali.kfs.pdp.batch.LoadPaymentsStep;
 import org.kuali.kfs.pdp.batch.SendAchAdviceNotificationsStep;
-import org.kuali.kfs.pdp.businessobject.ACHBank;
-import org.kuali.kfs.pdp.businessobject.CustomerProfile;
-import org.kuali.kfs.pdp.businessobject.PaymentDetail;
-import org.kuali.kfs.pdp.businessobject.PaymentFileLoad;
-import org.kuali.kfs.pdp.businessobject.PaymentGroup;
-import org.kuali.kfs.pdp.businessobject.PaymentNoteText;
+import org.kuali.kfs.pdp.businessobject.*;
 import org.kuali.kfs.pdp.service.AchBankService;
 import org.kuali.kfs.pdp.service.CustomerProfileService;
 import org.kuali.kfs.sys.KFSConstants;
+import org.kuali.kfs.sys.mail.BodyMailMessage;
+import org.kuali.kfs.sys.mail.MailMessage;
 import org.kuali.kfs.sys.report.BusinessObjectReportHelper;
+import org.kuali.kfs.sys.service.EmailService;
 import org.kuali.kfs.sys.service.impl.KfsParameterConstants;
 import org.kuali.rice.core.api.config.property.ConfigurationService;
-import org.kuali.kfs.sys.mail.MailMessage;
-import org.kuali.kfs.sys.mail.BodyMailMessage;
-import org.kuali.kfs.coreservice.impl.parameter.ParameterServiceImpl;
-import org.kuali.rice.kns.service.DataDictionaryService;
-import org.kuali.kfs.sys.service.EmailService;
-import org.kuali.kfs.krad.util.KRADUtils;
-
-import org.kuali.kfs.krad.util.ErrorMessage;
-import org.kuali.kfs.krad.util.MessageMap;
+import org.kuali.rice.core.web.format.CurrencyFormatter;
+import org.kuali.kfs.kns.service.DataDictionaryService;
 import org.kuali.rice.krad.exception.InvalidAddressException;
 import org.kuali.rice.krad.service.MailService;
-import org.kuali.rice.krad.util.ErrorMessage;
-import org.kuali.rice.krad.util.MessageMap;
 import org.kuali.rice.krad.util.ObjectUtils;
 
-import edu.arizona.kfs.pdp.service.PdpEmailService;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.*;
 
 public class PdpEmailServiceImpl extends org.kuali.kfs.pdp.service.impl.PdpEmailServiceImpl implements PdpEmailService {
     private static org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(PdpEmailServiceImpl.class);
 
     protected CustomerProfileService customerProfileService;
     protected ConfigurationService kualiConfigurationService;
-    protected MailService mailService;
+    protected EmailService emailService;
     protected ParameterService parameterService;
     protected DataDictionaryService dataDictionaryService;
     protected AchBankService achBankService;
     protected BusinessObjectReportHelper paymentDetailReportHelper;
     protected Set<String> knownEmailDomains;
 
-    public void sendPrepaidChecksLoadEmail(PaymentFileLoad prepaidChecksFile, List<String> warnings, String fileName) {    	
+    @SuppressWarnings("unchecked")
+    public void sendPrepaidChecksLoadEmail(PaymentFileLoad prepaidChecksFile, List<String> warnings, String fileName) {
         LOG.debug("sendPrepaidChecksLoadEmail() starting");
 
         // check email configuration
@@ -70,11 +53,11 @@ public class PdpEmailServiceImpl extends org.kuali.kfs.pdp.service.impl.PdpEmail
 
         String returnAddress = parameterService.getParameterValueAsString(KfsParameterConstants.PRE_DISBURSEMENT_BATCH.class, KFSConstants.FROM_EMAIL_ADDRESS_PARM_NM);
 
-//TODO: Natalia: release 30 KFS6->KFS7 merge: MailService no longer in KFS, no where to get BatchMailingList(); from... commenting out for now
-//TODO: sskinner, release 31, propagatnig the same
-//        if(StringUtils.isEmpty(returnAddress)) {
-//            returnAddress = emailService.getBatchMailingList();
-//        }
+        //TODO: Natalia: release 30 KFS6->KFS7 merge: MailService no longer in KFS, no where to get BatchMailingList(); from... commenting out for now
+        ////TODO: sskinner, release 31 KFS6->KFS7 merge; propagating the same
+        //if(StringUtils.isEmpty(returnAddress)) {
+        //    returnAddress = emailService.getBatchMailingList();
+        //}
 
         message.setFromAddress(returnAddress);
         message.setSubject("Prepaid Checks File Load Success Notification - " + fileName);
@@ -113,7 +96,7 @@ public class PdpEmailServiceImpl extends org.kuali.kfs.pdp.service.impl.PdpEmail
 
     }
 
-    public void sendPrepaidChecksErrorEmail(PaymentFileLoad prepaidChecksFile, MessageMap errors, String fileName) {    
+    public void sendPrepaidChecksErrorEmail(PaymentFileLoad prepaidChecksFile, org.kuali.kfs.krad.util.MessageMap errors, String fileName) {
         LOG.debug("sendPrepaidChecksErrorEmail() starting");
 
         // check email configuration
@@ -175,9 +158,9 @@ public class PdpEmailServiceImpl extends org.kuali.kfs.pdp.service.impl.PdpEmail
         }
 
         body.append("\n" + getMessage(PdpKeyConstants.MESSAGE_PAYMENT_EMAIL_ERROR_MESSAGES) + "\n");
-        List<ErrorMessage> errorMessages = errors.getMessages(KFSConstants.GLOBAL_ERRORS);
+        List<org.kuali.kfs.krad.util.ErrorMessage> errorMessages = errors.getMessages(KFSConstants.GLOBAL_ERRORS);
         if (errorMessages != null) {
-            for (ErrorMessage errorMessage : errorMessages) {
+            for (org.kuali.kfs.krad.util.ErrorMessage errorMessage : errorMessages) {
                 body.append(getMessage(errorMessage.getErrorKey(), (Object[]) errorMessage.getMessageParameters()) + "\n\n");
             }
 
@@ -202,18 +185,17 @@ public class PdpEmailServiceImpl extends org.kuali.kfs.pdp.service.impl.PdpEmail
      * KFSMI-6475 - Alter the subject and switch all recipients
      *
      * @param message
-     * @param environmentCode
      */
     @SuppressWarnings("rawtypes")
     public void alterMessageWhenNonProductionInstance( BodyMailMessage message ) {
-        if (! KRADUtils.isProductionEnvironment()) {
+        if (!KRADUtils.isProductionEnvironment()) {
             // insert the original recipients into the beginning of the message
             StringBuilder recipients = new StringBuilder();
             recipients.append("Intended To : ").append(message.getToAddresses().toString()).append('\n');
             recipients.append("Intended Cc : ").append(message.getCcAddresses().toString()).append('\n');
             recipients.append("Intended Bcc: ").append(message.getBccAddresses().toString()).append('\n');
             recipients.append('\n');
-            message.setMessage( recipients.toString() + message.getMessage() );
+            message.setMessage(recipients.toString() + message.getMessage());
             // Clear out the recipients
             message.setToAddresses(new HashSet());
             message.setCcAddresses(Collections.emptySet());
@@ -222,8 +204,9 @@ public class PdpEmailServiceImpl extends org.kuali.kfs.pdp.service.impl.PdpEmail
             // Set all to the batch mailing list
             //message.addToAddress(emailService.getBatchMailingList());
         }
+    }
 
-    @Override
+
     public void sendAchAdviceEmail(PaymentGroup paymentGroup, PaymentDetail paymentDetail, CustomerProfile customer) {
         LOG.debug("sendAchAdviceEmail() starting");
 
@@ -232,7 +215,7 @@ public class PdpEmailServiceImpl extends org.kuali.kfs.pdp.service.impl.PdpEmail
             return;
         }
 
-        MailMessage message = new MailMessage();
+        MailMessage message = new BodyMailMessage();
         String fromAddresses = customer.getAdviceReturnEmailAddr();
         String toAddresses = paymentGroup.getAdviceEmailAddress();
         Collection<String> ccAddresses = parameterService.getParameterValuesAsString(SendAchAdviceNotificationsStep.class, PdpParameterConstants.ACH_SUMMARY_CC_EMAIL_ADDRESSES_PARMAETER_NAME);
@@ -254,19 +237,21 @@ public class PdpEmailServiceImpl extends org.kuali.kfs.pdp.service.impl.PdpEmail
         }
 
         StringBuilder body = buildMessageBody(paymentGroup, customer);
-        message.setMessage(body.toString());
+        ((BodyMailMessage)message).setMessage(body.toString());
 
-        // KFSMI-6475 - if not a production instance, replace the recipients with the testers list
-        super.alterMessageWhenNonProductionInstance(message, null);
+        //TODO: sskinner, release 31 KFS6->KFS7 merge; This appears not to exist in v7 anymore... lets make sure this is ok
+        //super.alterMessageWhenNonProductionInstance(message, null);
 
         try {
         	checkEmailAddressDomain(paymentGroup.getAdviceEmailAddress());
-            mailService.sendMessage(message);
+            emailService.sendMessage(message, false);
         }
         catch (Exception e) {
             toAddresses = customer.getAdviceReturnEmailAddr();
             if(StringUtils.isEmpty(toAddresses)) {
-            	toAddresses = mailService.getBatchMailingList();
+
+                //TODO: sskinner, release 31 KFS6->KFS7 merge; this no longer exists, we need to find an alternative
+            	//toAddresses = emailService.getBatchMailingList();
             }
             message.setToAddresses(new HashSet());
             message.addToAddress(toAddresses);
@@ -274,17 +259,20 @@ public class PdpEmailServiceImpl extends org.kuali.kfs.pdp.service.impl.PdpEmail
 
             String returnAddress = parameterService.getParameterValueAsString(KfsParameterConstants.PRE_DISBURSEMENT_BATCH.class, KFSConstants.FROM_EMAIL_ADDRESS_PARM_NM);
             if(StringUtils.isEmpty(returnAddress)) {
-                returnAddress = mailService.getBatchMailingList();
+                //TODO: sskinner, release 31 KFS6->KFS7 merge; this no longer exists, we need to find an alternative
+                //returnAddress = emailService.getBatchMailingList();
             }
             message.setFromAddress(returnAddress);
             message.setSubject(getMessage(PdpKeyConstants.MESSAGE_PDP_ACH_ADVICE_INVALID_EMAIL_ADDRESS));
 
             LOG.debug("bouncing email to " + customer.getAdviceReturnEmailAddr() + " for disb # " + paymentGroup.getDisbursementNbr());
+
+            // TODO: sskinner, release 31 KFS6->KFS7 merge; this method no longer exists; make sure that's ok
             // KFSMI-6475 - if not a production instance, replace the recipients with the testers list
-            super.alterMessageWhenNonProductionInstance(message, null);
+            //super.alterMessageWhenNonProductionInstance(message, null);
 
             try {
-                mailService.sendMessage(message);
+                emailService.sendMessage(message, false);
             }
             catch (Exception e1) {
                 LOG.error("Could not send email to advice return email address on customer profile: " + customer.getAdviceReturnEmailAddr(), e1);
@@ -301,7 +289,7 @@ public class PdpEmailServiceImpl extends org.kuali.kfs.pdp.service.impl.PdpEmail
      */
     protected StringBuilder buildMessageBody(PaymentGroup paymentGroup, CustomerProfile customer) {
         Map<String, String> tableDefinition = paymentDetailReportHelper.getTableDefinition();
-        Formatter formatter = new CurrencyFormatter();
+        CurrencyFormatter formatter = new CurrencyFormatter();
 
         StringBuilder body = new StringBuilder();
         body.append(getMessage(PdpKeyConstants.MESSAGE_PDP_ACH_ADVICE_EMAIL_TOFROM, paymentGroup.getPayeeName(), customer.getAchPaymentDescription()));
@@ -370,9 +358,9 @@ public class PdpEmailServiceImpl extends org.kuali.kfs.pdp.service.impl.PdpEmail
         super.setConfigurationService(kualiConfigurationService);
     }
 
-    public void setMailService(MailService mailService) {
-        this.mailService = mailService;
-        super.setMailService(mailService);
+    public void setMailService(EmailService emailService) {
+        this.emailService = emailService;
+        super.setEmailService(emailService);
     }
     
     public void setParameterService(ParameterService parameterService) {
